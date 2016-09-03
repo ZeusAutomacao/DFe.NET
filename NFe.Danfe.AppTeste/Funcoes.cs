68527542f4f9167ba12692a23418a38168187c04
Adenilton B Silva <adenilton.bs@gmail.com>
2016-09-03 10:16:34 -0300

    Pojetos de impressão do DANFE adiciondo novamente na solução Zeus NFe.

---
+++ NFe.Danfe.AppTeste/Funcoes.cs
@@ -0,0 +1,92 @@
+﻿/********************************************************************************/
+/* Projeto: Biblioteca ZeusNFe                                                  */
+/* Biblioteca C# para emissão de Nota Fiscal Eletrônica - NFe e Nota Fiscal de  */
+/* Consumidor Eletrônica - NFC-e (http://www.nfe.fazenda.gov.br)                */
+/*                                                                              */
+/* Direitos Autorais Reservados (c) 2014 Adenilton Batista da Silva             */
+/*                                       Zeusdev Tecnologia LTDA ME             */
+/*                                                                              */
+/*  Você pode obter a última versão desse arquivo no GitHub                     */
+/* localizado em https://github.com/adeniltonbs/Zeus.Net.NFe.NFCe               */
+/*                                                                              */
+/*                                                                              */
+/*  Esta biblioteca é software livre; você pode redistribuí-la e/ou modificá-la */
+/* sob os termos da Licença Pública Geral Menor do GNU conforme publicada pela  */
+/* Free Software Foundation; tanto a versão 2.1 da Licença, ou (a seu critério) */
+/* qualquer versão posterior.                                                   */
+/*                                                                              */
+/*  Esta biblioteca é distribuída na expectativa de que seja útil, porém, SEM   */
+/* NENHUMA GARANTIA; nem mesmo a garantia implícita de COMERCIABILIDADE OU      */
+/* ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral Menor*/
+/* do GNU para mais detalhes. (Arquivo LICENÇA.TXT ou LICENSE.TXT)              */
+/*                                                                              */
+/*  Você deve ter recebido uma cópia da Licença Pública Geral Menor do GNU junto*/
+/* com esta biblioteca; se não, escreva para a Free Software Foundation, Inc.,  */
+/* no endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.          */
+/* Você também pode obter uma copia da licença em:                              */
+/* http://www.opensource.org/licenses/lgpl-license.php                          */
+/*                                                                              */
+/* Zeusdev Tecnologia LTDA ME - adenilton@zeusautomacao.com.br                  */
+/* http://www.zeusautomacao.com.br/                                             */
+/* Rua Comendador Francisco josé da Cunha, 111 - Itabaiana - SE - 49500-000     */
+/********************************************************************************/
+
+using System.Windows;
+using Microsoft.Win32;
+
+namespace NFe.Danfe.AppTeste
+{
+    public static class Funcoes
+    {
+        /// <summary>
+        ///     Exibe um diálogo com uma mensagem para o usuário, utilizando um ModernDialog
+        /// </summary>
+        /// <param name="mensagem"></param>
+        /// <param name="titulo"></param>
+        /// <param name="botoes"></param>
+        /// <param name="imagem"></param>
+        public static void Mensagem(string mensagem, string titulo, MessageBoxButton botoes, MessageBoxImage imagem = MessageBoxImage.None)
+        {
+            MessageBox.Show(mensagem, titulo, botoes, imagem);
+        }
+
+        /// <summary>
+        ///     Abre o diálogo de busca de arquivo com o filtro configurado para arquivos do tipo ".xml"
+        /// </summary>
+        /// <returns></returns>
+        public static string BuscarArquivoXml()
+        {
+            return BuscarArquivo("Selecione o arquivo XML", ".xml", "Arquivo XML (.xml)|*.xml");
+        }
+
+        /// <summary>
+        ///     Abre o diálogo de busca de arquivo com o filtro configurado para arquivos do tipo "PNG, Bitmap, JPEG, JPG e GIF"
+        /// </summary>
+        /// <returns></returns>
+        public static string BuscarImagem()
+        {
+            return BuscarArquivo("Selecione uma imagem", ".png", "PNG (*.png)|*.png|Bitmap (*.bmp)|*.bmp|JPEG (*.jpeg)|*.jpeg|JPG (*.jpg)|*.jpg|GIF (*.gif)|*.gif");
+        }
+
+        /// <summary>
+        ///     Abre o diálogo de busca de arquivo com com os dados passados no parâmetro
+        /// </summary>
+        /// <param name="arquivoPadrao">Nome do arquivo padrão a ser exibido no diálogo</param>
+        /// <param name="titulo">Título do diálogo de buscar arquivo</param>
+        /// <param name="extensaoPadrao">Extensão de arquivo padrão a ser exibida no diálogo</param>
+        /// <param name="filtro">Filtro de extensões a ser exibido no diálogo</param>
+        /// <returns></returns>
+        public static string BuscarArquivo(string titulo, string extensaoPadrao, string filtro, string arquivoPadrao = null)
+        {
+            var dlg = new OpenFileDialog
+            {
+                Title = titulo,
+                FileName = arquivoPadrao,
+                DefaultExt = extensaoPadrao,
+                Filter = filtro
+            };
+            dlg.ShowDialog();
+            return dlg.FileName;
+        }
+    }
+}
---
+++ NFe.Danfe.Fast/NFCe/DanfeFrNfce.cs
@@ -0,0 +1,91 @@
+﻿/********************************************************************************/
+/* Projeto: Biblioteca ZeusNFe                                                  */
+/* Biblioteca C# para emissão de Nota Fiscal Eletrônica - NFe e Nota Fiscal de  */
+/* Consumidor Eletrônica - NFC-e (http://www.nfe.fazenda.gov.br)                */
+/*                                                                              */
+/* Direitos Autorais Reservados (c) 2014 Adenilton Batista da Silva             */
+/*                                       Zeusdev Tecnologia LTDA ME             */
+/*                                                                              */
+/*  Você pode obter a última versão desse arquivo no GitHub                     */
+/* localizado em https://github.com/adeniltonbs/Zeus.Net.NFe.NFCe               */
+/*                                                                              */
+/*                                                                              */
+/*  Esta biblioteca é software livre; você pode redistribuí-la e/ou modificá-la */
+/* sob os termos da Licença Pública Geral Menor do GNU conforme publicada pela  */
+/* Free Software Foundation; tanto a versão 2.1 da Licença, ou (a seu critério) */
+/* qualquer versão posterior.                                                   */
+/*                                                                              */
+/*  Esta biblioteca é distribuída na expectativa de que seja útil, porém, SEM   */
+/* NENHUMA GARANTIA; nem mesmo a garantia implícita de COMERCIABILIDADE OU      */
+/* ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral Menor*/
+/* do GNU para mais detalhes. (Arquivo LICENÇA.TXT ou LICENSE.TXT)              */
+/*                                                                              */
+/*  Você deve ter recebido uma cópia da Licença Pública Geral Menor do GNU junto*/
+/* com esta biblioteca; se não, escreva para a Free Software Foundation, Inc.,  */
+/* no endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.          */
+/* Você também pode obter uma copia da licença em:                              */
+/* http://www.opensource.org/licenses/lgpl-license.php                          */
+/*                                                                              */
+/* Zeusdev Tecnologia LTDA ME - adenilton@zeusautomacao.com.br                  */
+/* http://www.zeusautomacao.com.br/                                             */
+/* Rua Comendador Francisco josé da Cunha, 111 - Itabaiana - SE - 49500-000     */
+/********************************************************************************/
+
+using System.IO;
+using FastReport;
+using FastReport.Barcode;
+using NFe.Classes;
+using NFe.Classes.Informacoes.Identificacao.Tipos;
+using NFe.Utils;
+using NFe.Utils.InformacoesSuplementares;
+
+namespace NFe.Danfe.Fast.NFCe
+{
+    /// <summary>
+    /// Classe reponsável pela impressão do DANFE da NFCe em Fast Reports
+    /// </summary>
+    public class DanfeFrNfce: DanfeBase
+    {
+        /// <summary>
+        /// Construtor da classe reponsável pela impressão do DANFE da NFCe em Fast Reports
+        /// </summary>
+        /// <param name="proc">Objeto do tipo nfeProc</param>
+        /// <param name="configuracaoDanfeNfce">Objeto do tipo ConfiguracaoDanfeNfce contendo as definições de impressão</param>
+        /// <param name="cIdToken">Identificador do CSC – Código de Segurança do Contribuinte no Banco de Dados da SEFAZ</param>
+        /// <param name="csc">Código de Segurança do Contribuinte(antigo Token)</param>
+        public DanfeFrNfce(nfeProc proc, ConfiguracaoDanfeNfce configuracaoDanfeNfce, string cIdToken, string csc)
+        {
+            #region Define as varíaveis que serão usadas no relatório (dúvidas a respeito do fast reports consulte a documentação em https://www.fast-report.com/pt/product/fast-report-net/documentation/)
+
+            Relatorio = new Report();
+            Relatorio.RegisterData(new[] { proc }, "NFCe", 20);
+            Relatorio.GetDataSource("NFCe").Enabled = true;
+            Relatorio.Load(new MemoryStream(Properties.Resources.NFCe));
+            Relatorio.SetParameterValue("NfceDetalheVendaNormal", configuracaoDanfeNfce.DetalheVendaNormal);
+            Relatorio.SetParameterValue("NfceDetalheVendaContigencia", configuracaoDanfeNfce.DetalheVendaContigencia);
+            Relatorio.SetParameterValue("NfceImprimeDescontoItem", configuracaoDanfeNfce.ImprimeDescontoItem);
+            ((ReportPage) Relatorio.FindObject("PgNfce")).LeftMargin = configuracaoDanfeNfce.MargemEsquerda;
+            ((ReportPage)Relatorio.FindObject("PgNfce")).RightMargin = configuracaoDanfeNfce.MargemDireita;
+            ((PictureObject) Relatorio.FindObject("poEmitLogo")).Image = configuracaoDanfeNfce.ObterLogo();
+            ((TextObject)Relatorio.FindObject("txtUrl")).Text = proc.NFe.infNFeSupl.ObterUrl(proc.NFe.infNFe.ide.tpAmb, proc.NFe.infNFe.ide.cUF, TipoUrlConsultaPublica.UrlConsulta);
+            ((BarcodeObject)Relatorio.FindObject("bcoQrCode")).Text = proc.NFe.infNFeSupl  == null ? proc.NFe.infNFeSupl.ObterUrlQrCode(proc.NFe, cIdToken, csc) : proc.NFe.infNFeSupl.qrCode;
+
+            //Segundo o Manual de Padrões Padrões Técnicos do DANFE - NFC - e e QR Code, versão 3.2, página 9, nos casos de emissão em contigência deve ser impresso uma segunda cópia como via do estabelecimento
+            Relatorio.PrintSettings.Copies = (proc.NFe.infNFe.ide.tpEmis == TipoEmissao.teNormal | NfeSituacao.Autorizada(proc.protNFe.infProt.cStat) /*Se a NFe for autorizada, mesmo que seja em contingência, imprime somente uma via*/ ) ? 1 : 2;
+
+            #endregion
+        }
+
+        /// <summary>
+        /// Construtor da classe reponsável pela impressão do DANFE da NFCe em Fast Reports.
+        /// Use esse construtor apenas para impressão em contigência, já que neste modo ainda não é possível obter o grupo protNFe 
+        /// </summary>
+        /// <param name="nfe">Objeto do tipo NFe</param>
+        /// <param name="configuracaoDanfeNfce">Objeto do tipo ConfiguracaoDanfeNfce contendo as definições de impressão</param>
+        /// <param name="cIdToken">Identificador do CSC – Código de Segurança do Contribuinte no Banco de Dados da SEFAZ</param>
+        /// <param name="csc">Código de Segurança do Contribuinte(antigo Token)</param>
+        public DanfeFrNfce(Classes.NFe nfe, ConfiguracaoDanfeNfce configuracaoDanfeNfce, string cIdToken, string csc) : this(new nfeProc() { NFe = nfe }, configuracaoDanfeNfce, cIdToken, csc)
+        {
+        }
+    }
+}
