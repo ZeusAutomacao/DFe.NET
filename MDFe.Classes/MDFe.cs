755382d02da5a5377b4b91edd562bde8eed2771d
robertorp <robertoalvespereira18@gmail.com>
2016-11-11 12:01:22 +0000

    add

--- MDFe.Classes/MDFe.cs
+++ MDFe.Classes/MDFe.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MDFe.Classes
+﻿namespace MDFe.Classes
 {
-    class MDFe
+    public class MDFe
     {
     }
 }
