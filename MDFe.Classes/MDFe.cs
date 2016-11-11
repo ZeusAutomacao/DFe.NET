d268afa3585c6c52cdef100a47bf2ff0c08a2be7
robertorp <robertoalvespereira18@gmail.com>
2016-11-11 12:24:06 +0000

    copiado classes da nfe assinatura para dfe.classes, não foi deletado as classes da nfe.classes por questão de compatibilidade com aplicações legadas

---
+++ MDFe.Classes/MDFe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Xml.Serialization;
+using DFe.Classes.Assinatura;
+
+namespace MDFe.Classes
+{
+    [Serializable]
+    [XmlRoot(Namespace = "http://www.portalfiscal.inf.br/mdfe",
+        ElementName = "MDFe")]
+    public class MDFe
+    {
+        [XmlElement(ElementName = "infMDFe")]
+        public MDFeInfMDFe InfMdFe { get; set; }
+
+        public Signature Signature { get; set; }
+    }
+}
--- MDFe.Classes/MDFeInfMDFe.cs
+++ MDFe.Classes/MDFeInfMDFe.cs
@@ -4,8 +4,6 @@ using System.Xml.Serialization;
 namespace MDFe.Classes
 {
     [Serializable]
-    [XmlRoot(Namespace = "http://www.portalfiscal.inf.br/cte",
-        ElementName = "infMDFe")]
     public class MDFeInfMDFe
     {
         [XmlAttribute(AttributeName = "versao")]
