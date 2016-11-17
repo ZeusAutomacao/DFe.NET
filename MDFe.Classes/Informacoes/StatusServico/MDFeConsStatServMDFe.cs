efc7316c9552f7c188e44b03a9a5aa45f099dc1c
robertorp <robertoalvespereira18@gmail.com>
2016-11-17 17:42:17 -0200

    "Adicionado classes de consulta status serviço"

---
+++ MDFe.Classes/Informacoes/StatusServico/MDFeConsStatServMDFe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Xml.Serialization;
+using DFe.Classes.Flags;
+using ManifestoDocumentoFiscalEletronico.Classes.Servicos.Flags;
+
+namespace ManifestoDocumentoFiscalEletronico.Classes.Informacoes.StatusServico
+{
+    [Serializable]
+    [XmlRoot(Namespace = "http://www.portalfiscal.inf.br/mdfe",
+        ElementName = "consStatServMDFe")]
+    public class MDFeConsStatServMDFe
+    {
+        [XmlAttribute(AttributeName = "versao")]
+        public VersaoServico Versao { get; set; }
+
+        [XmlElement(ElementName = "TpAmb")]
+        public TipoAmbiente TpAmb { get; set; }
+
+        [XmlElement(ElementName = "xServ")]
+        public string XServ { get; set; }
+    }
+}
---
+++ MDFe.Classes/Retorno/MDFeStatusServico/MDFeRetConsStatServ.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Xml.Serialization;
+using DFe.Classes.Entidades;
+using DFe.Classes.Extencoes;
+using DFe.Classes.Flags;
+using ManifestoDocumentoFiscalEletronico.Classes.Servicos.Flags;
+
+namespace ManifestoDocumentoFiscalEletronico.Classes.Retorno.MDFeStatusServico
+{
+    [Serializable]
+    [XmlRoot(Namespace = "http://www.portalfiscal.inf.br/mdfe",
+        ElementName = "retConsStatServMDFe")]
+    public class MDFeRetConsStatServ
+    {
+        [XmlAttribute(AttributeName = "versao")]
+        public string Versao { get; set; }
+
+        [XmlElement(ElementName = "tpAmb")]
+        public TipoAmbiente TpAmb { get; set; }
+
+        [XmlElement(ElementName = "verAplic")]
+        public string VerAplic { get; set; }
+
+        [XmlElement(ElementName = "cStat")]
+        public short CStat { get; set; }
+
+        [XmlElement(ElementName = "xMotivo")]
+        public string XMotivo { get; set; }
+
+        [XmlIgnore]
+        public EstadoUF CUF { get; set; }
+
+        [XmlElement(ElementName = "cUF")]
+        public string CUFProxy
+        {
+            get
+            {
+                return CUF.GetCodigoIbgeEmString();
+            }
+            set { CUF = CUF.CodigoIbgeParaEstado(value); }
+        }
+
+        [XmlElement(ElementName = "dhRecbto")]
+        public DateTime DhRecbto { get; set; }
+
+        [XmlElement(ElementName = "tMed")]
+        public int? TMed { get; set; }
+
+        [XmlElement(ElementName = "dhRetorno")]
+        public DateTime? DhRetorno { get; set; }
+
+        [XmlElement(ElementName = "xObs")]
+        public string XObs { get; set; }
+
+        public bool TMedSpecified => TMed.HasValue;
+        public bool DhRetornoSpecified => DhRetorno.HasValue;
+    }
+}
