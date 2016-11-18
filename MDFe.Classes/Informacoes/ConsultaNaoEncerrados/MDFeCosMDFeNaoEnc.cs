54fe1c9fa115d3d67a484b074f681ce68a402d0c
robertorp <robertoalvespereira18@gmail.com>
2016-11-18 08:18:02 -0200

    "Feito classes base para consulta não encerrados"

---
+++ MDFe.Classes/Informacoes/ConsultaNaoEncerrados/MDFeCosMDFeNaoEnc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Xml.Serialization;
+using DFe.Classes.Flags;
+using ManifestoDocumentoFiscalEletronico.Classes.Servicos.Flags;
+
+namespace ManifestoDocumentoFiscalEletronico.Classes.Informacoes.ConsultaNaoEncerrados
+{
+    [Serializable]
+    [XmlRoot(Namespace = "http://www.portalfiscal.inf.br/mdfe",
+        ElementName = "consMDFeNaoEnc")]
+    public class MDFeCosMDFeNaoEnc
+    {
+        [XmlAttribute(AttributeName = "versao")]
+        public VersaoServico Versao { get; set; }
+
+        [XmlElement(ElementName = "tpAmb")]
+        public TipoAmbiente TpAmb { get; set; }
+
+        [XmlElement(ElementName = "xServ")]
+        public string XServ { get; set; } = "CONSULTAR NÃO ENCERRADOS";
+
+        [XmlElement(ElementName = "CNPJ")]
+        public string CNPJ { get; set; }
+    }
+}
---
+++ MDFe.Classes/Retorno/MDFeConsultaNaoEncerrado/MDFeRetConsMDFeNao.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using DFe.Classes.Entidades;
+using DFe.Classes.Extencoes;
+using DFe.Classes.Flags;
+using ManifestoDocumentoFiscalEletronico.Classes.Servicos.Flags;
+
+namespace ManifestoDocumentoFiscalEletronico.Classes.Retorno.MDFeConsultaNaoEncerrado
+{
+    [Serializable]
+    [XmlRoot(Namespace = "http://www.portalfiscal.inf.br/mdfe",
+        ElementName = "retConsMDFeNaoEnc")]
+    public class MDFeRetConsMDFeNao
+    {
+        [XmlAttribute(AttributeName = "versao")]
+        public VersaoServico Versao { get; set; }
+
+        [XmlElement(ElementName = "tpAmb")]
+        public TipoAmbiente TpAmb { get; set; }
+
+        [XmlElement(ElementName = "verAplic")]
+        public string VerAplic { get; set; }
+
+        [XmlElement(ElementName = "cStat")]
+        public short CStat { get; set; }
+
+        [XmlElement(ElementName = "xMotivo")]
+        public string XMotivo { get; set; }
+
+        [XmlIgnore]
+        public EstadoUF CUF { get; set; }
+
+        [XmlElement(ElementName = "cUF")]
+        public string CUFProxy
+        {
+            get
+            {
+                return CUF.GetCodigoIbgeEmString();
+            }
+            set { CUF = CUF.CodigoIbgeParaEstado(value); }
+        }
+
+        [XmlElement(ElementName = "infMDFe")]
+        public List<MDFeNaoEncerradaInfMDFe> InfMDFe { get; set; }
+    }
+
+    [Serializable]
+    public class MDFeNaoEncerradaInfMDFe
+    {
+        [XmlElement(ElementName = "chMDFe")]
+        public string ChMDFe { get; set; }
+        [XmlElement(ElementName = "nProt")]
+        public string NProt { get; set; }
+    }
+}
