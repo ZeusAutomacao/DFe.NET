b5a40780f1ced3b7159d5b98b284c7f568ddc5e9
robertorp <robertoalvespereira18@gmail.com>
2016-12-03 09:34:54 -0200

    Adicionado ProcMDFe.cs

---
+++ MDFe.Classes/Retorno/MDFeProcMDFe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Xml.Serialization;
+using ManifestoDocumentoFiscalEletronico.Classes.Informacoes;
+using ManifestoDocumentoFiscalEletronico.Classes.Retorno.MDFeRetRecepcao;
+using ManifestoDocumentoFiscalEletronico.Classes.Servicos.Flags;
+
+namespace ManifestoDocumentoFiscalEletronico.Classes.Retorno
+{
+    [Serializable]
+    [XmlRoot(ElementName = "mdfeProc", Namespace = "http://www.portalfiscal.inf.br/mdfe")]
+    public class MDFeProcMDFe
+    {
+        [XmlAttribute(AttributeName = "versao")]
+        public VersaoServico Versao { get; set; }
+
+        [XmlElement(ElementName = "MDFe", Namespace = "http://www.portalfiscal.inf.br/mdfe")]
+        public MDFe MDFe { get; set; }
+
+        [XmlElement(ElementName = "protMDFe")]
+        public MDFeProtMDFe ProtMDFe { get; set; }
+    }
+}
