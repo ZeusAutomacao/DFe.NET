69ed52d8eb92b6564dbb13e4dfa89ba9b2f7e261
Adenilton <adenilton.bs@gmail.com>
2017-01-02 17:36:23 -0300

    * Alterações conforme issue https://github.com/adeniltonbs/Zeus.Net.NFe.NFCe/issues/251.

--- NFe.Classes/Informacoes/Detalhe/Tributacao/Municipal/ISSQN.cs
+++ NFe.Classes/Informacoes/Detalhe/Tributacao/Municipal/ISSQN.cs
@@ -128,7 +128,7 @@ namespace NFe.Classes.Informacoes.Detalhe.Tributacao.Municipal
         /// <summary>
         ///     U12 - Indicador da exigibilidade do ISS
         /// </summary>
-        public IndicadorISS IndicadorIss { get; set; }
+        public IndicadorISS indISS{ get; set; }
 
         /// <summary>
         ///     U13 - Código do serviço prestado dentro do município
