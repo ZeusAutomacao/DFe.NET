7ff09f0355e01da2dceebd70d38a21fa726a8b89
Adenilton <adenilton.bs@gmail.com>
2017-01-13 09:19:17 -0300

    * Adicionada documentação xml para classe NFe.Classes.Servicos.Inutilizacao.procInutNFe e seus atributos. Complemento para o P/R https://github.com/adeniltonbs/Zeus.Net.NFe.NFCe/pull/330.

--- NFe.Classes/Servicos/Inutilizacao/procInutNFe.cs
+++ NFe.Classes/Servicos/Inutilizacao/procInutNFe.cs
@@ -34,14 +34,26 @@ using System.Xml.Serialization;
 
 namespace NFe.Classes.Servicos.Inutilizacao
 {
+    /// <summary>
+    /// Pedido de inutilização de numeração de NF-e processado
+    /// </summary>
     [XmlRoot(Namespace = "http://www.portalfiscal.inf.br/nfe")]
     public class procInutNFe
     {
+        /// <summary>
+        /// Versão do pedido de inutilização
+        /// </summary>
         [XmlAttribute]
         public string versao { get; set; }
 
+        /// <summary>
+        /// Mensagem de solicitação de inutilização de numeração de NFe. 
+        /// </summary>
         public inutNFe inutNFe { get; set; }
 
+        /// <summary>
+        /// Mensagem de retorno da solicitação de inutilização de numeração de NF-e.
+        /// </summary>
         public retInutNFe retInutNFe { get; set; }
     }
 }
