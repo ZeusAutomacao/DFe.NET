64b330d808dc6b000800e122dbd0f17d89a56ef2
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2017-02-19 21:42:18 -0300

    Emitente para 2.00 é 3.00

--- CTe.Classes/Informacoes/Emitente/emit.cs
+++ CTe.Classes/Informacoes/Emitente/emit.cs
@@ -1,26 +1,20 @@
-﻿using System;
-using System.Xml.Serialization;
-
-namespace CTeDLL.Classes.Informacoes.Emitente
+﻿namespace CTeDLL.Classes.Informacoes.Emitente
 {
     public class emit
     {
-        public enderEmit enderEmit;
+        public string CNPJ { get; set; }
 
-        private string _CNPJ;
-        private string _IE;
-        private string _xNome;
-        private string _xFant;
+        public string IE { get; set; }
 
-        public string CNPJ { get { return _CNPJ; } set { _CNPJ = value; } }
-        public string IE { get { return _IE; } set { _IE = value; } }
-        public string xNome { get { return _xNome; } set { _xNome = value; } }
-        public string xFant { get { return _xFant; } set { _xFant = value; } }
-        
         /// <summary>
-        ///     C21 - Código de Regime Tributário
+        /// Versão 3.00 - Não é obrigatório
         /// </summary>
-        public CRT CRT { get; set; }
+        public string IEST { get; set; }
 
+        public string xNome { get; set; }
+
+        public string xFant { get; set; }
+
+        public enderEmit enderEmit { get; set; }
     }
 }
--- CTe.Classes/Informacoes/Emitente/enderEmit.cs
+++ CTe.Classes/Informacoes/Emitente/enderEmit.cs
@@ -1,28 +1,49 @@
-﻿using System;
-using System.Xml.Serialization;
+﻿using System.Xml.Serialization;
+using DFe.Classes.Entidades;
+using DFe.Classes.Extencoes;
 
 namespace CTeDLL.Classes.Informacoes.Emitente
 {
     public class enderEmit
     {
-        private string _xLgr;
-        private string _nro;
-        private string _xCpl;
-        private string _xBairro;
-        private int _cMun;
-        private string _xMun;
-        private string _UF;
-        private string _CEP;
-        private string _fone;
+        public string xLgr { get; set; }
 
-        public string xLgr { get { return _xLgr; } set { _xLgr = value; } }
-        public string nro { get { return _nro; } set { _nro = value; } }
-        public string xCpl { get { return _xCpl; } set { _xCpl = value; } }
-        public string xBairro { get { return _xBairro; } set { _xBairro = value; } }
-        public int cMun { get { return _cMun; } set { _cMun = value; } }
-        public string xMun { get { return _xMun; } set { _xMun = value; } }
-        public string UF { get { return _UF; } set { _UF = value; } }
-        public string CEP { get { return _CEP; } set { _CEP = value; } }
-        public string fone { get { return _fone; } set { _fone = value; } }
+        public string nro { get; set; }
+
+        public string xCpl { get; set; }
+
+        public string xBairro { get; set; }
+
+        public long cMun { get; set; }
+
+        public string xMun { get; set; }
+
+        /// <summary>
+        /// 3 - CEP
+        /// </summary>
+        [XmlIgnore]
+        public long CEP { get; set; }
+
+        /// <summary>
+        /// Proxy para colocar zeros a esquerda no CEP 
+        /// </summary>
+        [XmlElement(ElementName = "CEP")]
+        public string ProxyCEP
+        {
+            get { return CEP.ToString("D8"); }
+            set { CEP = long.Parse(value); }
+        }
+
+        [XmlIgnore]
+        public Estado UF { get; set; }
+
+        [XmlElement(ElementName = "UF")]
+        public string ProxyUF
+        {
+            get { return UF.GetSiglaUfString(); }
+            set { UF = UF.SiglaParaEstado(value); }
+        }
+
+        public string fone { get; set; }
     }
 }
