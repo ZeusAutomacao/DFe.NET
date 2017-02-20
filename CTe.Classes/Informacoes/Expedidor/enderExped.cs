13e75ff62e39b62aa55600663a871be561cc22b8
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2017-02-19 22:09:51 -0300

    Ajustado expedidor para 2.00 é 3.00

--- CTe.Classes/Informacoes/Expedidor/enderExped.cs
+++ CTe.Classes/Informacoes/Expedidor/enderExped.cs
@@ -1,34 +1,54 @@
 ﻿using System;
 using System.Xml.Serialization;
+using DFe.Classes.Entidades;
+using DFe.Classes.Extencoes;
 
 namespace CTeDLL.Classes.Informacoes.Expedidor
 {
     public class enderExped
     {
-        private string _xLgr;
-        private string _nro;
-        private string _xCpl;
-        private string _xBairro;
-        private int _cMun;
-        private string _xMun;
-        private string _UF;
-        private string _CEP;
-        private string _fone;
-        private int _cPais;
-        private string _xPais;
-        private string _email;
+        public string xLgr { get; set; }
 
-        public string xLgr { get { return _xLgr; } set { _xLgr = value; } }
-        public string nro { get { return _nro; } set { _nro = value; } }
-        public string xCpl { get { return _xCpl; } set { _xCpl = value; } }
-        public string xBairro { get { return _xBairro; } set { _xBairro = value; } }
-        public int cMun { get { return _cMun; } set { _cMun = value; } }
-        public string xMun { get { return _xMun; } set { _xMun = value; } }
-        public string UF { get { return _UF; } set { _UF = value; } }
-        public string CEP { get { return _CEP; } set { _CEP = value; } }
-        public string fone { get { return _fone; } set { _fone = value; } }
-        public int cPais { get { return _cPais; } set { _cPais = value; } }
-        public string xPais { get { return _xPais; } set { _xPais = value; } }
-        public string email { get { return _email; } set { _email = value; } }
+        public string nro { get; set; }
+
+        public string xCpl { get; set; }
+
+        public string xBairro { get; set; }
+
+        public int cMun { get; set; }
+
+        public string xMun { get; set; }
+
+        /// <summary>
+        /// 3 - CEP
+        /// </summary>
+        [XmlIgnore]
+        public long CEP { get; set; }
+
+        /// <summary>
+        /// Proxy para colocar zeros a esquerda no CEP 
+        /// </summary>
+        [XmlElement(ElementName = "CEP")]
+        public string ProxyCEP
+        {
+            get { return CEP.ToString("D8"); }
+            set { CEP = long.Parse(value); }
+        }
+
+        [XmlIgnore]
+        public Estado UF { get; set; }
+
+        [XmlElement(ElementName = "UF")]
+        public string ProxyUF
+        {
+            get { return UF.GetSiglaUfString(); }
+            set { UF = UF.SiglaParaEstado(value); }
+        }
+
+        public int? cPais { get; set; }
+
+        public bool cPaisSpecified { get { return cPais.HasValue; } }
+
+        public string xPais { get; set; }
     }
 }
