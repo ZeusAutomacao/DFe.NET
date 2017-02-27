7e04700bd59dc81e990afb862ef41eca1aed7db3
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2017-02-27 11:38:42 -0300

    criado base de app teste

---
+++ CTe.AppTeste/Entidades/ConfigWebService.cs
@@ -0,0 +1,19 @@
+using System;
+using CTeDLL.Classes.Servicos.Tipos;
+using DFe.Classes.Entidades;
+using DFe.Classes.Flags;
+
+namespace CTe.AppTeste.Entidades
+{
+    [Serializable]
+    public class ConfigWebService
+    {
+        public Estado UfEmitente { get; set; }
+        public TipoAmbiente Ambiente { get; set; }
+        public short Serie { get; set; }
+        public long Numeracao { get; set; }
+        public versao Versao { get; set; }
+        public string CaminhoSchemas { get; set; }
+        public int TimeOut { get; set; }
+    }
+}
