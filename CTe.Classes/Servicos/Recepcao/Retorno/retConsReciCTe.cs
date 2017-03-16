cd6ed8117d362d9c3b251dcf8f74eb77a9c353c1
robertorp <robertoalvespereira18@gmail.com>
2017-03-15 23:58:31 -0300

    Adicionado licença de uso

--- CTe.Classes/Servicos/Recepcao/Retorno/retConsReciCTe.cs
+++ CTe.Classes/Servicos/Recepcao/Retorno/retConsReciCTe.cs
@@ -38,64 +38,81 @@ using DFe.Classes.Entidades;
 using DFe.Classes.Flags;
 using DFe.Utils;
 
-namespace CTeDLL.Classes.Servicos.Consulta
+namespace CTeDLL.Classes.Servicos.Recepcao.Retorno
 {
     [XmlRoot(Namespace = "http://www.portalfiscal.inf.br/cte")]
-    public class retConsSitCTe : RetornoBase
+    public class retConsReciCTe : RetornoBase
     {
         /// <summary>
-        ///     ER02 - Versão do leiaute
+        ///     BR02 - Versão do leiaute
         /// </summary>
         [XmlAttribute]
         public versao versao { get; set; }
 
         /// <summary>
-        ///     ER03 - Identificação do Ambiente: 1 – Produção / 2 – Homologação
+        ///     Identificação do Ambiente: 1 – Produção / 2 - Homologação
         /// </summary>
         public TipoAmbiente tpAmb { get; set; }
 
         /// <summary>
-        ///     Versão do Aplicativo que processou a consulta. A versão deve ser iniciada com a sigla da UF nos casos de WS próprio
-        ///     ou a sigla SCAN, SVAN ou SVRS nos demais casos
+        ///     Versão do Aplicativo que recebeu a Consulta. A versão deve ser iniciada com a sigla da UF nos casos de WS próprio
+        ///     ou a sigla SCAN, SVAN ou SVRS nos demais casos.
         /// </summary>
         public string verAplic { get; set; }
 
         /// <summary>
-        ///     ER05 - Código do status da resposta.
+        ///     BR04a - Número do Recibo consultado. Será preenchido com zeros se for impossível de obter o valor da mensagem de
+        ///     entrada (Ex. mensagem inválida).
+        /// </summary>
+        public string nRec { get; set; }
+
+        /// <summary>
+        ///     BR05 - Se cStatus = 215, 516, 517 ou 545 significa que a mensagem de consulta é inválida.
+        ///     Se cStatus = 225, 565. 567 ou 568, significa que o lote de NF-e consultado é inválido
         /// </summary>
         public int cStat { get; set; }
 
         /// <summary>
-        ///     ER06 - Descrição literal do status da resposta.
+        ///     BR06 - Descrição literal do status da resposta.
         /// </summary>
         public string xMotivo { get; set; }
 
         /// <summary>
-        ///     ER07 - Código da UF que atendeu a solicitação
+        ///     BR06a - Código da UF que atendeu a solicitação.
         /// </summary>
         public Estado cUF { get; set; }
 
         /// <summary>
-        ///     ER08 - Protocolo de autorização ou denegação de uso da CT-e (vide item 4.2.2).
-        ///     Informar se localizado uma CT-e com cStat = 100 (uso autorizado) ou 110 (uso denegado).
+        ///     BR06b - Código da Mensagem (v2.0) Campo de uso da SEFAZ para enviar mensagem de interesse da SEFAZ para o emissor.
+        ///     (NT 2011/004)
         /// </summary>
-        public protCTe protCTe { get; set; }
+        public int? cMsg { get; set; }
 
         /// <summary>
-        ///     ER10 - Informação do evento e respectivo Protocolo de registro de Evento
+        ///     BR06c - Mensagem da SEFAZ para o emissor. (v2.0)
         /// </summary>
-        [XmlElement("procEventoCTe")]
-        public List<procEventoCTe> procEventoCTe { get; set; }
+        public string xMsg { get; set; }
 
+        /// <summary>
+        ///     BR07 - Conjunto de resultado do processamento de cada NF-e (vide leiaute abaixo).
+        ///     Estas informações são retornadas apenas para o código do status do lote = 104 (Lote processado)
+        /// </summary>
+        [XmlElement("protCTe")]
+        public List<protCTe> protCTe { get; set; }
 
-        public static retConsSitCTe LoadXml(string xml)
+        public bool ShouldSerializecMsg()
         {
-            var retorno = FuncoesXml.XmlStringParaClasse<retConsSitCTe>(xml);
+            return cMsg.HasValue;
+        }
+
+        public static retConsReciCTe LoadXml(string xml)
+        {
+            var retorno = FuncoesXml.XmlStringParaClasse<retConsReciCTe>(xml);
             retorno.RetornoXmlString = xml;
             return retorno;
         }
 
-        public static retConsSitCTe LoadXml(string xml, consSitCTe consSitCTe)
+        public static retConsReciCTe LoadXml(string xml, consReciCTe consSitCTe)
         {
             var retorno = LoadXml(xml);
             retorno.EnvioXmlString = FuncoesXml.ClasseParaXmlString(consSitCTe);
