5a20807ce3b5110795f980c0cde5a389c9246490
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2017-05-11 09:33:13 -0300

    Cache de certificado digital alterado para aceitar mais de um, quando um sistema for multiempresa você tem a possibilidade de ter mais de um certificado digital em cache no Zeus.
    Por padrão o zeus pega o SerialNumber do certificado e adiciona numa lista. Isso somente serve para uso de um unico certificado digital. Para usar mais de um você deve setar na classe ConfiguracaoCertificado.CacheId = id de sua preferência.

--- NFe.AppTeste/MainWindow.xaml.cs
+++ NFe.AppTeste/MainWindow.xaml.cs
@@ -126,6 +126,8 @@ namespace NFe.AppTeste
 
                 //Exemplo com using para chamar o método Dispose da classe.
                 //Usar dessa forma, especialmente, quando for usar certificado A3 com a senha salva.
+                // se usar cache você pode por um id no certificado e salvar mais de um certificado digital também na memoria com o zeus
+                //_configuracoes.CfgServico.Certificado.CacheId = "1";
                 using (var servicoNFe = new ServicosNFe(_configuracoes.CfgServico))
                 {
                     var retornoStatus = servicoNFe.NfeStatusServico();
