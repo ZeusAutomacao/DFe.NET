dc6fe686f909dbe72dd981b390e0c328fcc5214f
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2017-06-02 10:34:08 -0300

    Alterado url MDFeConsNaoEnc de produção

---
+++ MDFe.Wsdl/Gerado/MDFeRecepcao/mdfeCabecMsg.cs
@@ -0,0 +1,10 @@
+﻿using System.Web.Services.Protocols;
+
+namespace MDFe.Wsdl.Gerado.MDFeRecepcao.Teste
+{
+    public class mdfeCabecMsg : SoapHeader
+    {
+        public string cUF { get; set; }
+        public string versaoDados { get; set; }
+    }
+}
