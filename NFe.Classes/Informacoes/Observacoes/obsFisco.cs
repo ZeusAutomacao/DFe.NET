744fdbfd5e5e7a32743ca2bde88880f95fe9d6c9
Adriano Barros Silva <adrbarros@gmail.com>
2017-06-22 11:13:49 -0300

    Alterações

--- NFe.Classes/Informacoes/Observacoes/obsFisco.cs
+++ NFe.Classes/Informacoes/Observacoes/obsFisco.cs
@@ -30,6 +30,7 @@
 /* http://www.zeusautomacao.com.br/                                             */
 /* Rua Comendador Francisco josé da Cunha, 111 - Itabaiana - SE - 49500-000     */
 /********************************************************************************/
+using System.Xml.Serialization;
 
 namespace NFe.Classes.Informacoes.Observacoes
 {
@@ -38,6 +39,7 @@ namespace NFe.Classes.Informacoes.Observacoes
         /// <summary>
         ///     Z08 - Identificação do campo
         /// </summary>
+        [XmlAttribute]
         public string xCampo { get; set; }
 
         /// <summary>
