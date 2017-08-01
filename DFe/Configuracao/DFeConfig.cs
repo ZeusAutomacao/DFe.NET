e92263d8440b6e7d9e8829a2fc48f0f5c925ae6c
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2017-07-31 23:00:10 -0300

    Criado um DFeConfig padrão

---
+++ DFe/Configuracao/DFeConfig.cs
@@ -0,0 +1,26 @@
+﻿using DFe.Classes.Entidades;
+using DFe.Classes.Flags;
+using DFe.MDFe.Classes.Flags;
+using DFe.Utils;
+
+namespace DFe.Configuracao
+{
+    public abstract class DFeConfig
+    {
+        public DFeConfig()
+        {
+            ConfiguracaoCertificado = new ConfiguracaoCertificado();
+        }
+
+        public ConfiguracaoCertificado ConfiguracaoCertificado { get; }
+
+        public bool IsSalvarXml { get; set; }
+        public string CaminhoSchemas { get; set; }
+        public string CaminhoSalvarXml { get; set; }
+        public string TimeOut { get; set; }
+
+        public abstract TipoAmbiente TipoAmbiente { get; set; }
+        public abstract VersaoServico VersaoServico { get; set; }
+        public abstract Estado EstadoUf { get; set; }
+    }
+}
