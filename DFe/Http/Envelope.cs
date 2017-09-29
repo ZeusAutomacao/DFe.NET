db0654431fb491cd80cd1c8398e58f4181f9fb2c
robertorp <robertoalvespereira18@gmail.com>
2017-09-29 13:53:03 -0300

    Feito classe para Fazer request na sefaz compativel com .net standard é .net framework

---
+++ DFe/Http/Envelope.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Xml;
+
+namespace DFe.Http
+{
+    public class Envelope
+    {
+        public string Construir(XmlNode xml)
+        {
+            StringBuilder env = new StringBuilder("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+
+
+            env.Append("<soap12:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" ");
+            env.Append("xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" ");
+            env.Append("xmlns:soap12=\"http://www.w3.org/2003/05/soap-envelope\">");
+
+            env.Append("<soap12:Header>");
+            env.Append("<nfeCabecMsg xmlns=\"http://www.portalfiscal.inf.br/nfe/wsdl/NfeStatusServico\">");
+            env.Append("<cUF>29</cUF>");
+            env.Append("<versaoDados>3.10</versaoDados>");
+            env.Append("</nfeCabecMsg>");
+            env.Append("</soap12:Header>");
+
+            env.Append("<soap12:Body>");
+
+            env.Append("<nfeDadosMsg xmlns=\"http://www.portalfiscal.inf.br/nfe/wsdl/NfeStatusServico\">");
+            env.Append(xml.LastChild.OuterXml.ToString());
+            env.Append("</nfeDadosMsg>");
+
+            env.Append("</soap12:Body>");
+            env.Append("</soap12:Envelope>");
+
+            return env.ToString();
+        }
+    }
+}
---
+++ DFe/Http/RequestWS.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Xml;
+using DFe.Http.Ext;
+
+namespace DFe.Http
+{
+    public class RequestWS
+    {
+        public string EnviaSefaz(XmlNode xml, string url, string metodo)
+        {
+            try
+            {
+                string XMLRetorno = string.Empty;
+                string xmlSoap = new Envelope().Construir(xml);
+
+                Uri uri = new Uri(url);
+
+                WebRequest webRequest = WebRequest.Create(uri);
+                HttpWebRequest httpWR = (HttpWebRequest)webRequest;
+                // todo httpWR.Timeout
+
+                httpWR.ContentLength = Encoding.ASCII.GetBytes(xmlSoap).Length;
+
+                httpWR.ClientCertificates.Add(new X509Certificate2(@"certificado", "senha do mesmo"));
+
+                httpWR.ComposeContentType("application/soap+xml", Encoding.UTF8, metodo);
+
+                httpWR.Method = "POST";
+
+                Stream reqStream = httpWR.GetRequestStream();
+                StreamWriter streamWriter = new StreamWriter(reqStream);
+                streamWriter.Write(xmlSoap, 0, Encoding.ASCII.GetBytes(xmlSoap).Length);
+                streamWriter.Close();
+
+                WebResponse webResponse = httpWR.GetResponse();
+                Stream respStream = webResponse.GetResponseStream();
+                StreamReader streamReader = new StreamReader(respStream);
+
+                XMLRetorno = streamReader.ReadToEnd();
+
+                return XMLRetorno;
+            }
+            catch (WebException ex)
+            {
+                using (var stream = ex.Response.GetResponseStream())
+                using (var reader = new StreamReader(stream))
+                {
+                    Console.WriteLine(reader.ReadToEnd());
+                }
+                throw;
+            }
+        }
+    }
+}
