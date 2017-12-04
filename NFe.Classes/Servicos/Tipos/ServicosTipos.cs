20a163809d4dbd729093a169025341d2de28ab07
Felipe Rodrigues Jensen <felipe.jensen@iatec.com>
2017-12-04 16:14:58 -0200

    Adicionado tipo de serviço 4;00

--- NFe.Classes/Servicos/Tipos/ServicosTipos.cs
+++ NFe.Classes/Servicos/Tipos/ServicosTipos.cs
@@ -125,7 +125,8 @@ namespace NFe.Classes.Servicos.Tipos
     {
         [XmlEnum("1.00")] ve100,
         [XmlEnum("2.00")] ve200,
-        [XmlEnum("3.10")] ve310
+        [XmlEnum("3.10")] ve310,
+        [XmlEnum("4.00")] ve400
     }
 
     /// <summary>
