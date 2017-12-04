8d7b176b74a6d2eb5ca6641a8671391127d6b8a3
Felipe Rodrigues Jensen <felipe.jensen@iatec.com>
2017-12-04 16:16:44 -0200

    Adicionada a classe de serviço de status 4.00

---
+++ NFe.Wsdl/Status/NfeStatusServico4.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Web.Services;
+using System.Web.Services.Description;
+using System.Web.Services.Protocols;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace NFe.Wsdl.Status
+{
+    [System.Web.Services.WebServiceBindingAttribute(Name = "NFeStatusServico4Soap12", Namespace = "http://www.portalfiscal.inf.br/nfe/wsdl/NFeStatusServico4")]
+    public class NfeStatusServico4 : SoapHttpClientProtocol, INfeServico
+    {
+        public NfeStatusServico4(string url, X509Certificate certificado, int timeOut)
+        {
+            SoapVersion = SoapProtocolVersion.Soap12;
+            Url = url;
+            Timeout = timeOut;
+            ClientCertificates.Add(certificado);
+        }
+
+        public nfeCabecMsg nfeCabecMsg
+        {
+            get => throw new NotImplementedException();
+            set { }
+        }
+
+        [SoapDocumentMethod("http://www.portalfiscal.inf.br/nfe/wsdl/NFeStatusServico4/nfeStatusServicoNF", Use = SoapBindingUse.Literal, ParameterStyle = SoapParameterStyle.Bare)]
+        [WebMethod(MessageName = "nfeStatusServicoNF")]
+        [return: XmlElement("nfeResultMsg", Namespace = "http://www.portalfiscal.inf.br/nfe/wsdl/NFeStatusServico4")]
+        public XmlNode Execute([XmlElement(Namespace = "http://www.portalfiscal.inf.br/nfe/wsdl/NFeStatusServico4")] XmlNode nfeDadosMsg)
+        {
+            var results = Invoke("nfeStatusServicoNF", new object[] { nfeDadosMsg });
+            return ((XmlNode)(results[0]));
+        }
+    }
+}
