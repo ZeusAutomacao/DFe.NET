26e310f1490872a48157308d1e032b44de9c92ac
Michel Balthazar <michel@nfe.io>
2018-04-17 11:16:09 -0300

    Add dfe.util's using on infoProt

--- NFe.Classes/Protocolo/infProt.cs
+++ NFe.Classes/Protocolo/infProt.cs
@@ -33,6 +33,7 @@
 using System;
 using System.Xml.Serialization;
 using DFe.Classes.Assinatura;
+using DFe.Utils;
 using NFe.Classes.Informacoes.Identificacao.Tipos;
 
 namespace NFe.Classes.Protocolo
