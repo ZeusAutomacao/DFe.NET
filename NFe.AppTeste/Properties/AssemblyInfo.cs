966242b0b6d4e6d3b91aa0482866dbd46824567c
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2018-05-07 11:30:34 -0300

    trocado versão dos assemblys

--- NFe.AppTeste/Properties/AssemblyInfo.cs
+++ NFe.AppTeste/Properties/AssemblyInfo.cs
@@ -84,5 +84,5 @@ using System.Windows;
 // utilizando o '*' como mostrado abaixo:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.670")]
-[assembly: AssemblyFileVersion("1.0.0.670")]
+[assembly: AssemblyVersion("1.0.0.671")]
+[assembly: AssemblyFileVersion("1.0.0.671")]
