d9165db0238c166917678a203027b75366969fd6
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2018-05-08 17:31:41 -0300

    correção em urls mt 4.00

--- NFe.AppTeste/MainWindow.xaml.cs
+++ NFe.AppTeste/MainWindow.xaml.cs
@@ -95,7 +95,7 @@ namespace NFe.AppTeste
 
         public MainWindow()
         {
-            // todo ServicePointManager.ServerCertificateValidationCallback = delegate { return true; }; // não precisa de cadeia de certificado digital 
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; }; // não precisa de cadeia de certificado digital 
             InitializeComponent();
             CarregarConfiguracao();
             DataContext = _configuracoes;
