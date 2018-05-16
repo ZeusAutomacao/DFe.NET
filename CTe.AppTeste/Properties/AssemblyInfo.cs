3e53872e4367ac60000b7d131bfa2ca84d227153
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2018-05-16 09:54:18 -0300

    alterado versão assemblys

--- CTe.AppTeste/Properties/AssemblyInfo.cs
+++ CTe.AppTeste/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.676")]
-[assembly: AssemblyFileVersion("1.0.0.676")]
+[assembly: AssemblyVersion("1.0.0.677")]
+[assembly: AssemblyFileVersion("1.0.0.677")]
