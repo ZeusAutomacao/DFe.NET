07c46e544f5f3a2cc09071bbcdab078555de477e
Felipe Jensen <rodrigues_felipe7@hotmail.com>
2018-06-19 10:18:41 -0300

    Adicionado projeto para serviços homologação são paulo

---
+++ NFe.Wsdl.Sp.Hom/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFe.Wsdl.Sp.Hom
+{
+    public class Class1
+    {
+    }
+}
