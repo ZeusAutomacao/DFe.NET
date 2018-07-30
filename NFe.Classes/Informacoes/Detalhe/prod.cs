1b55b7d8a398d9e9e6f98db38e576c0a9cbfde81
Felipe Jensen <felipe.jensen@iatec.com>
2018-07-30 09:18:05 -0300

    Atualizado o valor de casas decimais de VUnTrib para 10.

--- NFe.Classes/Informacoes/Detalhe/prod.cs
+++ NFe.Classes/Informacoes/Detalhe/prod.cs
@@ -168,7 +168,7 @@ namespace NFe.Classes.Informacoes.Detalhe
         public decimal vUnTrib
         {
             get { return _vUnTrib; }
-            set { _vUnTrib = value.Arredondar(2); }
+            set { _vUnTrib = value.Arredondar(10); }
         }
 
         /// <summary>
