155ec6ad92d361a27f0db953bf7794bbf5e2fd0d
Michel Balthazar <michel@nfe.io>
2018-08-21 15:47:15 -0300

    refactor ExtinfNFeSupl

--- NFe.Classes/Enums.cs
+++ NFe.Classes/Enums.cs
@@ -43,4 +43,13 @@ namespace NFe.Classes
         [Description("Endereço para consulta da NFCe através do QR-Code")]
         UrlQrCode
     }
+
+    public enum VersaoQrCode
+    {
+        [Description("Versão 1.0 do QR-Code")]
+        QrCodeVersao1 = 100,
+
+        [Description("Versão 2.0 do QR-Code")]
+        QrCodeVersao2 = 2
+    }
 }
