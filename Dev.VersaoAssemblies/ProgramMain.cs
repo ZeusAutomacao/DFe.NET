e7fe8d1a9c196e835cb966c83ad3c30b7b2173fb
Eliezer Almeida <eliezeralmeidago@hotmail.com>
2018-08-28 16:53:10 -0300

    adiciondo projeto para ajudar a alterar versão dos assemblies

---
+++ Dev.VersaoAssemblies/ProgramMain.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Dev.VersaoAssemblies
+{
+    internal static class ProgramMain
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Olá, sou seu assitente para alteração de versão em massa");
+                Console.WriteLine("Para continuar preciso que informe qual versão deseja utilizar. Vamos la?");
+
+                Console.Write("Então qual versão quer utilizar? (informe a versãoe aperter ENTER):");
+                var versaoString = Console.ReadLine();
+
+                FazerAlteracaoDasVersoes(versaoString);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro: " + ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+
+        private static void FazerAlteracaoDasVersoes(string versaoString)
+        {
+            var root = Environment.GetEnvironmentVariable("PROJETO_ZEUS_DFE") ?? string.Empty;
+
+            if (!File.Exists(Path.Combine(root, "Zeus NFe.sln")))
+            {
+                throw new InvalidOperationException(
+                    "Acho que você não está com a variavel de ambiente PROJETO_ZEUS_DFE configurada");
+            }
+
+            var diretorios = Directory.GetDirectories(root);
+
+            foreach (var diretorio in diretorios)
+            {
+                var infoFile = Path.Combine(diretorio, "Properties", "AssemblyInfo.cs");
+
+                if (!File.Exists(infoFile))
+                {
+                    continue;
+                }
+
+                var regexValidarVersao = new Regex(@"^[0-9]\.[0-9]{1,2}\.0\.[0-9]{1,2}$");
+
+                if (!regexValidarVersao.IsMatch(versaoString))
+                {
+                    throw new InvalidOperationException(
+                        "Opa, você não informou uma versão que eu esperava. Preciso de algo assim: X.X.0.XX?");
+                }
+
+                AlterarVersaoDoAssembly(infoFile, versaoString);
+                Console.WriteLine("Alterado versão no AssemblyInfo: " + infoFile);
+            }
+
+            Console.WriteLine("Pronto terminei. Pode apertar qualquer tecla para fechar!");
+        }
+
+        private static void AlterarVersaoDoAssembly(string infoFile, string versaoString)
+        {
+            var content = File.ReadAllText(infoFile);
+
+            var regexVersion = new Regex("assembly: AssemblyVersion.+\\)", RegexOptions.IgnoreCase);
+            var regexFileVersion = new Regex("assembly: AssemblyFileVersion.+\\)", RegexOptions.IgnoreCase);
+
+            var novoContent = regexVersion.Replace(content, "assembly: AssemblyVersion(\"" + versaoString + "\")");
+            var contentFinal = regexFileVersion.Replace(novoContent, "assembly: AssemblyFileVersion(\"" + versaoString + "\")");
+
+            File.WriteAllText(infoFile, contentFinal);
+        }
+    }
+}
---
+++ Dev.VersaoAssemblies/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("Dev.VersaoAssemblies")]
+[assembly: AssemblyDescription("Fusion 2017")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Agil4 Tecnologia")]
+[assembly: AssemblyProduct("Sistema Fusion")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("Agil4")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("35429d4d-65df-47f5-93be-bfbf35fbbf3b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("6.1.0.7")]
+
+[assembly: AssemblyVersion("6.1.0.7")]
+[assembly: AssemblyFileVersion("6.1.0.7")]
