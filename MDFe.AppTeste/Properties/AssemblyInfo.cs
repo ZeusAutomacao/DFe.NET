316aedc0a0adbfc480c7d78cbeed64c32f5c32ab
Eliezer Almeida <eliezeralmeidago@hotmail.com>
2018-08-29 16:42:15 -0300

    alterado versão dos assemblies

--- MDFe.AppTeste/Properties/AssemblyInfo.cs
+++ MDFe.AppTeste/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.0.727")]
-[assembly: AssemblyVersion("1.0.0.727")]
-[assembly: AssemblyFileVersion("1.0.0.727")]
+// [assembly: AssemblyVersion("1.0.0.728")]
+[assembly: AssemblyVersion("1.0.0.728")]
+[assembly: AssemblyFileVersion("1.0.0.728")]
