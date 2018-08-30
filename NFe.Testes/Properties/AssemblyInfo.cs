ca8ab4c77662cada8beb2ad92e49996605907553
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2018-08-30 10:07:12 -0300

    alterado versão dos assemblys

--- NFe.Testes/Properties/AssemblyInfo.cs
+++ NFe.Testes/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("3e44ea28-96ae-4fac-9e38-adc2e12930f1")]
 
-// [assembly: AssemblyVersion("1.0.0.727")]
-[assembly: AssemblyVersion("1.0.0.727")]
-[assembly: AssemblyFileVersion("1.0.0.727")]
+// [assembly: AssemblyVersion("1.0.0.728")]
+[assembly: AssemblyVersion("1.0.0.728")]
+[assembly: AssemblyFileVersion("1.0.0.728")]
