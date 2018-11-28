0b679303dc2a374928d294b9cf591b3da4e215e4
Anderson Jordão Marques <anjomar@gmail.com>
2018-11-28 13:45:05 -0200

    Adicionando correção de acentuação simples.
    Teste

--- NFe.Servicos/ServicosNFe.cs
+++ NFe.Servicos/ServicosNFe.cs
@@ -540,6 +540,7 @@ namespace NFe.Servicos
                 detEvento.xCondUso =
                     "A Carta de Correcao e disciplinada pelo paragrafo 1o-A do art. 7o do Convenio S/N, de 15 de dezembro de 1970 e pode ser utilizada para regularizacao de erro ocorrido na emissao de documento fiscal, desde que o erro nao esteja relacionado com: I - as variaveis que determinam o valor do imposto tais como: base de calculo, aliquota, diferenca de preco, quantidade, valor da operacao ou da prestacao; II - a correcao de dados cadastrais que implique mudanca do remetente ou do destinatario; III - a data de emissao ou de saida.";
                 detEvento.descEvento = "Carta de Correcao";
+                detEvento.xCorrecao = correcao.RemoverAcentucaoSimples();
             }
 
             var infEvento = new infEventoEnv
