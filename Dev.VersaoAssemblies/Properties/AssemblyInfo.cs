04ac32e3a06d5b03c4b671ee8754148dd13d68ee
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2019-03-09 14:17:46 -0300

    alterado versão dos assemblys para gerar versão

--- Dev.VersaoAssemblies/Properties/AssemblyInfo.cs
+++ Dev.VersaoAssemblies/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.735")]
-[assembly: AssemblyFileVersion("1.0.0.735")]
+[assembly: AssemblyVersion("1.0.0.736")]
+[assembly: AssemblyFileVersion("1.0.0.736")]
