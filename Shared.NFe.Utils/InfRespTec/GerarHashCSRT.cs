3c6fe692b31586ccddd3a2e18260c290ba7b3957
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2019-03-12 17:10:26 -0300

    Adicionado infRespTec e classe auxiliar para gerar a HashCSRT Nome da classe "GerarHashCSRT" basta usar o método statico e preencher o csrt e chave da nfe

---
+++ Shared.NFe.Utils/InfRespTec/GerarHashCSRT.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Shared.NFe.Utils.InfRespTec
+{
+    public static class GerarHashCSRT
+    {
+        public static string HashCSRT(string csrt, string chave, Encoding encoding = null)
+        {
+            if (encoding == null)
+            {
+                encoding = Encoding.UTF8;
+            }
+
+            var csrtChave = new StringBuilder(csrt).Append(chave).ToString();
+
+            var hash = Hash(csrtChave, encoding);
+
+            var hashCSRT = Convert.ToBase64String(encoding.GetBytes(hash));
+
+            return hashCSRT;
+        }
+
+        private static string Hash(string input, Encoding encoding)
+        {
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(encoding.GetBytes(input));
+
+                var sb = new StringBuilder(hash.Length * 2);
+                foreach (var b in hash)
+                {
+                    sb.Append(b.ToString("x2"));
+                }
+
+                return sb.ToString(0, 20);
+            }
+        }
+    }
+}
