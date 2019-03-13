f721272a88fc9d4d5c8e26e43bf2440d460ece92
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2019-03-13 09:34:38 -0300

    alterado versão dos assemblys para lançar nuget

--- NFe.Danfe.AppTeste/Properties/AssemblyInfo.cs
+++ NFe.Danfe.AppTeste/Properties/AssemblyInfo.cs
@@ -48,6 +48,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.0.736")]
-[assembly: AssemblyVersion("1.0.0.736")]
-[assembly: AssemblyFileVersion("1.0.0.736")]
+// [assembly: AssemblyVersion("1.0.0.737")]
+[assembly: AssemblyVersion("1.0.0.737")]
+[assembly: AssemblyFileVersion("1.0.0.737")]
--- NFe.Testes/Properties/AssemblyInfo.cs
+++ NFe.Testes/Properties/AssemblyInfo.cs
@@ -14,6 +14,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("3e44ea28-96ae-4fac-9e38-adc2e12930f1")]
 
-// [assembly: AssemblyVersion("1.0.0.736")]
-[assembly: AssemblyVersion("1.0.0.736")]
-[assembly: AssemblyFileVersion("1.0.0.736")]
+// [assembly: AssemblyVersion("1.0.0.737")]
+[assembly: AssemblyVersion("1.0.0.737")]
+[assembly: AssemblyFileVersion("1.0.0.737")]
