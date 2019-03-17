2fad68331bb317d19956b9348485c2ec09cea5e8
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2019-03-17 01:14:45 -0300

    alterado versão dos assemblys

--- Dev.VersaoAssemblies/Properties/AssemblyInfo.cs
+++ Dev.VersaoAssemblies/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.738")]
-[assembly: AssemblyFileVersion("1.0.0.738")]
+[assembly: AssemblyVersion("1.0.0.739")]
+[assembly: AssemblyFileVersion("1.0.0.739")]
--- NFe.AppTeste/Properties/AssemblyInfo.cs
+++ NFe.AppTeste/Properties/AssemblyInfo.cs
@@ -82,7 +82,7 @@ using System.Windows;
 //
 // É possível especificar todos os valores ou usar o padrão de Números de Compilação e Revisão 
 // utilizando o '*' como mostrado abaixo:
-// [assembly: AssemblyVersion("1.0.0.738")]
+// [assembly: AssemblyVersion("1.0.0.739")]
 
-[assembly: AssemblyVersion("1.0.0.738")]
-[assembly: AssemblyFileVersion("1.0.0.738")]
+[assembly: AssemblyVersion("1.0.0.739")]
+[assembly: AssemblyFileVersion("1.0.0.739")]
