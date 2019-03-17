46d90acbfd7e4e16b15ba7c2a5e6953cd7f50419
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2019-03-17 01:28:35 -0300

    Adicionado infFretamento para modal rodoviarioOs

---
+++ CTe.Classes/Informacoes/infCTeNormal/infModals/rodoviarioOS/tpFretamento.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml.Serialization;
+
+namespace CTe.Classes.Informacoes.infCTeNormal.infModals.rodoviarioOS
+{
+    public enum tpFretamento
+    {
+        [XmlEnum("1")]
+        Eventual = 1,
+
+        [XmlEnum("2")]
+        Continuo = 2
+    }
+}
