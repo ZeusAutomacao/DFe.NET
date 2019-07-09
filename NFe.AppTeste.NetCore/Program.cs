df79c0f832396b6f994867e92590c279fa418025
DaniloBreda <danilo@bredas.com.br>
2019-07-09 20:37:19 -0300

    criado projetos

---
+++ NFe.AppTeste.NetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NFe.AppTeste.NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
