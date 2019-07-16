7d72c440efb832bdee5433be6979435856377995
DaniloBreda <danilo@bredas.com.br>
2019-07-16 00:09:56 -0300

    arrumado erro de UTF8

--- Shared.NFe.Danfe.Base/IDanfe.cs
+++ Shared.NFe.Danfe.Base/IDanfe.cs
@@ -1,34 +1,34 @@
 /********************************************************************************/
 /* Projeto: Biblioteca ZeusNFe                                                  */
-/* Biblioteca C# para emiss�o de Nota Fiscal Eletr�nica - NFe e Nota Fiscal de  */
-/* Consumidor Eletr�nica - NFC-e (http://www.nfe.fazenda.gov.br)                */
+/* Biblioteca C# para emissão de Nota Fiscal Eletrônica - NFe e Nota Fiscal de  */
+/* Consumidor Eletrônica - NFC-e (http://www.nfe.fazenda.gov.br)                */
 /*                                                                              */
 /* Direitos Autorais Reservados (c) 2014 Adenilton Batista da Silva             */
 /*                                       Zeusdev Tecnologia LTDA ME             */
 /*                                                                              */
-/*  Voc� pode obter a �ltima vers�o desse arquivo no GitHub                     */
+/*  Você pode obter a última versão desse arquivo no GitHub                     */
 /* localizado em https://github.com/adeniltonbs/Zeus.Net.NFe.NFCe               */
 /*                                                                              */
 /*                                                                              */
-/*  Esta biblioteca � software livre; voc� pode redistribu�-la e/ou modific�-la */
-/* sob os termos da Licen�a P�blica Geral Menor do GNU conforme publicada pela  */
-/* Free Software Foundation; tanto a vers�o 2.1 da Licen�a, ou (a seu crit�rio) */
-/* qualquer vers�o posterior.                                                   */
+/*  Esta biblioteca é software livre; você pode redistribuí-la e/ou modificá-la */
+/* sob os termos da Licença Pública Geral Menor do GNU conforme publicada pela  */
+/* Free Software Foundation; tanto a versão 2.1 da Licença, ou (a seu critério) */
+/* qualquer versão posterior.                                                   */
 /*                                                                              */
-/*  Esta biblioteca � distribu�da na expectativa de que seja �til, por�m, SEM   */
-/* NENHUMA GARANTIA; nem mesmo a garantia impl�cita de COMERCIABILIDADE OU      */
-/* ADEQUA��O A UMA FINALIDADE ESPEC�FICA. Consulte a Licen�a P�blica Geral Menor*/
-/* do GNU para mais detalhes. (Arquivo LICEN�A.TXT ou LICENSE.TXT)              */
+/*  Esta biblioteca é distribuída na expectativa de que seja útil, porém, SEM   */
+/* NENHUMA GARANTIA; nem mesmo a garantia implícita de COMERCIABILIDADE OU      */
+/* ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral Menor*/
+/* do GNU para mais detalhes. (Arquivo LICENÇA.TXT ou LICENSE.TXT)              */
 /*                                                                              */
-/*  Voc� deve ter recebido uma c�pia da Licen�a P�blica Geral Menor do GNU junto*/
-/* com esta biblioteca; se n�o, escreva para a Free Software Foundation, Inc.,  */
-/* no endere�o 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.          */
-/* Voc� tamb�m pode obter uma copia da licen�a em:                              */
+/*  Você deve ter recebido uma cópia da Licença Pública Geral Menor do GNU junto*/
+/* com esta biblioteca; se não, escreva para a Free Software Foundation, Inc.,  */
+/* no endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.          */
+/* Você também pode obter uma copia da licença em:                              */
 /* http://www.opensource.org/licenses/lgpl-license.php                          */
 /*                                                                              */
 /* Zeusdev Tecnologia LTDA ME - adenilton@zeusautomacao.com.br                  */
 /* http://www.zeusautomacao.com.br/                                             */
-/* Rua Comendador Francisco jos� da Cunha, 111 - Itabaiana - SE - 49500-000     */
+/* Rua Comendador Francisco josé da Cunha, 111 - Itabaiana - SE - 49500-000     */
 /********************************************************************************/
 
 using System.IO;
@@ -38,10 +38,10 @@ namespace NFe.Danfe.Base
     public interface IDanfe
     {
         /// <summary>
-        /// Envia a impress�o do DANFE da NFCe diretamente para a impressora
+        /// Envia a impressão do DANFE da NFCe diretamente para a impressora
         /// </summary>
-        /// <param name="exibirDialogo">Se true exibe o di�logo Imprimindo...</param>
-        /// <param name="impressora">Passe a string com o nome da impressora para imprimir diretamente em determinada impressora. Caso contr�rio, a impress�o ser� feita na impressora que estiver como padr�o</param>
+        /// <param name="exibirDialogo">Se true exibe o diálogo Imprimindo...</param>
+        /// <param name="impressora">Passe a string com o nome da impressora para imprimir diretamente em determinada impressora. Caso contrário, a impressão será feita na impressora que estiver como padrão</param>
         void Imprimir(bool exibirDialogo = true, string impressora = "");
 
         /// <summary>
