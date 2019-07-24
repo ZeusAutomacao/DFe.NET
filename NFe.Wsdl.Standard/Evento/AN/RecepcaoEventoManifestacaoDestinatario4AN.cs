c840e05c2f5cdb6e006eb6dceed954a6099dc96d
DaniloBreda <danilo@bredas.com.br>
2019-07-24 01:57:18 -0300

    Migrado eventos da nfe

---
+++ NFe.Wsdl.Standard/Evento/AN/RecepcaoEventoManifestacaoDestinatario4AN.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace NFe.Wsdl.Evento.AN
+{
+    public class RecepcaoEventoManifestacaoDestinatario4AN : RecepcaoEvento4AN
+    {
+        public RecepcaoEventoManifestacaoDestinatario4AN(string url, X509Certificate certificado, int timeOut) : base(url, certificado, timeOut)
+        {
+        }
+    }
+}
