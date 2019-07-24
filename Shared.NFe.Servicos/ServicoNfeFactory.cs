cf15572c7eb143dda6a765303febaff4532b7567
DaniloBreda <danilo@bredas.com.br>
2019-07-24 01:25:17 -0300

    criado classes de distribuicaodfe e download (ainda não implementadas)

--- Shared.NFe.Servicos/ServicoNfeFactory.cs
+++ Shared.NFe.Servicos/ServicoNfeFactory.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Reflection;
-using System.Security.Cryptography.X509Certificates;
-using DFe.Classes.Entidades;
+﻿using DFe.Classes.Entidades;
 using DFe.Classes.Flags;
-using DFe.DocumentosEletronicos.Wsdl;
-using DFe.DocumentosEletronicos.Wsdl.Cabecalho;
-using DFe.DocumentosEletronicos.Wsdl.Corpo;
-using DFe.Wsdl;
 using NFe.Classes.Servicos.Tipos;
 using NFe.Servicos.Extensoes;
 using NFe.Utils;
@@ -19,8 +12,8 @@ using NFe.Wsdl.Autorizacao.SVCAN;
 using NFe.Wsdl.ConsultaProtocolo;
 using NFe.Wsdl.ConsultaProtocolo.SVAN;
 using NFe.Wsdl.ConsultaProtocolo.SVCAN;
-//using NFe.Wsdl.DistribuicaoDFe;
-//using NFe.Wsdl.Download;
+using NFe.Wsdl.DistribuicaoDFe;
+using NFe.Wsdl.Download;
 //using NFe.Wsdl.Evento;
 //using NFe.Wsdl.Evento.AN;
 //using NFe.Wsdl.Evento.SVAN;
@@ -31,6 +24,8 @@ using NFe.Wsdl.ConsultaProtocolo.SVCAN;
 using NFe.Wsdl.Status;
 using NFe.Wsdl.Status.SVAN;
 using NFe.Wsdl.Status.SVCAN;
+using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace NFe.Servicos
 {
@@ -110,7 +105,7 @@ namespace NFe.Servicos
                     {
                         return new NfeStatusServico4NFeSVCAN(url, certificado, cfg.TimeOut);
                     }
-                    
+
                     if (cfg.cUF == Estado.PR & cfg.VersaoNfeStatusServico == VersaoServico.Versao310)
                     {
                         return new NfeStatusServico3(url, certificado, cfg.TimeOut);
@@ -157,104 +152,104 @@ namespace NFe.Servicos
 
                     return new NfeConsultaProtocolo2(url, certificado, cfg.TimeOut);
 
-                 /*
-                case ServicoNFe.NfeRecepcao:
-                    return new NfeRecepcao2(url, certificado, cfg.TimeOut);
+                /*
+               case ServicoNFe.NfeRecepcao:
+                   return new NfeRecepcao2(url, certificado, cfg.TimeOut);
 
-                case ServicoNFe.NfeRetRecepcao:
-                    return new NfeRetRecepcao2(url, certificado, cfg.TimeOut);
+               case ServicoNFe.NfeRetRecepcao:
+                   return new NfeRetRecepcao2(url, certificado, cfg.TimeOut);
 
-                case ServicoNFe.NFeAutorizacao:
-                    throw new Exception(string.Format("O serviço {0} não pode ser criado no método {1}!", servico,
-                        MethodBase.GetCurrentMethod().Name));
+               case ServicoNFe.NFeAutorizacao:
+                   throw new Exception(string.Format("O serviço {0} não pode ser criado no método {1}!", servico,
+                       MethodBase.GetCurrentMethod().Name));
 
-                case ServicoNFe.NFeRetAutorizacao:
-                    if (cfg.UsaSvanNFe4(cfg.VersaoNFeRetAutorizacao))
-                    {
-                        return new NfeRetAutorizacao4SVAN(url, certificado, cfg.TimeOut);
-                    }
+               case ServicoNFe.NFeRetAutorizacao:
+                   if (cfg.UsaSvanNFe4(cfg.VersaoNFeRetAutorizacao))
+                   {
+                       return new NfeRetAutorizacao4SVAN(url, certificado, cfg.TimeOut);
+                   }
 
-                    if (cfg.UsaSvcanNFe4(cfg.VersaoNFeRetAutorizacao))
-                    {
-                        return new NfeRetAutorizacao4SVCAN(url, certificado, cfg.TimeOut);
-                    }
+                   if (cfg.UsaSvcanNFe4(cfg.VersaoNFeRetAutorizacao))
+                   {
+                       return new NfeRetAutorizacao4SVCAN(url, certificado, cfg.TimeOut);
+                   }
 
-                    if (cfg.VersaoNFeRetAutorizacao == VersaoServico.Versao400)
-                    {
-                        return new NfeRetAutorizacao4(url, certificado, cfg.TimeOut);
-                    }
+                   if (cfg.VersaoNFeRetAutorizacao == VersaoServico.Versao400)
+                   {
+                       return new NfeRetAutorizacao4(url, certificado, cfg.TimeOut);
+                   }
 
-                    if (cfg.cUF == Estado.PR & cfg.VersaoNFeAutorizacao == VersaoServico.Versao310)
-                    {
-                        return new NfeRetAutorizacao3(url, certificado, cfg.TimeOut);
-                    }
+                   if (cfg.cUF == Estado.PR & cfg.VersaoNFeAutorizacao == VersaoServico.Versao310)
+                   {
+                       return new NfeRetAutorizacao3(url, certificado, cfg.TimeOut);
+                   }
 
-                    return new NfeRetAutorizacao(url, certificado, cfg.TimeOut);
+                   return new NfeRetAutorizacao(url, certificado, cfg.TimeOut);
 
-                case ServicoNFe.NfeInutilizacao:
+               case ServicoNFe.NfeInutilizacao:
 
-                    if (cfg.UsaSvanNFe4(cfg.VersaoNfeInutilizacao))
-                    {
-                        return new NFeInutilizacao4SVAN(url, certificado, cfg.TimeOut);
-                    }
+                   if (cfg.UsaSvanNFe4(cfg.VersaoNfeInutilizacao))
+                   {
+                       return new NFeInutilizacao4SVAN(url, certificado, cfg.TimeOut);
+                   }
 
-                    if (cfg.VersaoNfeInutilizacao == VersaoServico.Versao400)
-                    {
-                        return new NFeInutilizacao4(url, certificado, cfg.TimeOut);
-                    }
+                   if (cfg.VersaoNfeInutilizacao == VersaoServico.Versao400)
+                   {
+                       return new NFeInutilizacao4(url, certificado, cfg.TimeOut);
+                   }
 
-                    if (cfg.cUF == Estado.PR & cfg.VersaoNfeInutilizacao == VersaoServico.Versao310)
-                    {
-                        return new NfeInutilizacao3(url, certificado, cfg.TimeOut);
-                    }
+                   if (cfg.cUF == Estado.PR & cfg.VersaoNfeInutilizacao == VersaoServico.Versao310)
+                   {
+                       return new NfeInutilizacao3(url, certificado, cfg.TimeOut);
+                   }
 
-                    if (cfg.cUF == Estado.BA & cfg.VersaoNfeInutilizacao == VersaoServico.Versao310 & cfg.ModeloDocumento == ModeloDocumento.NFe)
-                    {
-                        return new NfeInutilizacao(url, certificado, cfg.TimeOut);
-                    }
+                   if (cfg.cUF == Estado.BA & cfg.VersaoNfeInutilizacao == VersaoServico.Versao310 & cfg.ModeloDocumento == ModeloDocumento.NFe)
+                   {
+                       return new NfeInutilizacao(url, certificado, cfg.TimeOut);
+                   }
 
-                    return new NfeInutilizacao2(url, certificado, cfg.TimeOut);
+                   return new NfeInutilizacao2(url, certificado, cfg.TimeOut);
 
-                case ServicoNFe.RecepcaoEventoCancelmento:
-                case ServicoNFe.RecepcaoEventoCartaCorrecao:
-                    if (cfg.UsaSvanNFe4(cfg.VersaoRecepcaoEventoCceCancelamento))
-                    {
-                        return new RecepcaoEvento4SVAN(url, certificado, cfg.TimeOut);
-                    }
+               case ServicoNFe.RecepcaoEventoCancelmento:
+               case ServicoNFe.RecepcaoEventoCartaCorrecao:
+                   if (cfg.UsaSvanNFe4(cfg.VersaoRecepcaoEventoCceCancelamento))
+                   {
+                       return new RecepcaoEvento4SVAN(url, certificado, cfg.TimeOut);
+                   }
 
-                    if (cfg.UsaSvcanNFe4(cfg.VersaoRecepcaoEventoCceCancelamento))
-                    {
-                        return new RecepcaoEvento4SVCAN(url, certificado, cfg.TimeOut);
-                    }
+                   if (cfg.UsaSvcanNFe4(cfg.VersaoRecepcaoEventoCceCancelamento))
+                   {
+                       return new RecepcaoEvento4SVCAN(url, certificado, cfg.TimeOut);
+                   }
 
-                    if (cfg.VersaoRecepcaoEventoCceCancelamento == VersaoServico.Versao400)
-                    {
-                        return new RecepcaoEvento4(url, certificado, cfg.TimeOut);
-                    }
+                   if (cfg.VersaoRecepcaoEventoCceCancelamento == VersaoServico.Versao400)
+                   {
+                       return new RecepcaoEvento4(url, certificado, cfg.TimeOut);
+                   }
 
-                    return new RecepcaoEvento(url, certificado, cfg.TimeOut);
+                   return new RecepcaoEvento(url, certificado, cfg.TimeOut);
 
-                case ServicoNFe.RecepcaoEventoManifestacaoDestinatario:
-                    {
-                        if (cfg.VersaoRecepcaoEventoManifestacaoDestinatario == VersaoServico.Versao400)
-                        {
-                            return new RecepcaoEventoManifestacaoDestinatario4AN(url, certificado, cfg.TimeOut);
-                        }
+               case ServicoNFe.RecepcaoEventoManifestacaoDestinatario:
+                   {
+                       if (cfg.VersaoRecepcaoEventoManifestacaoDestinatario == VersaoServico.Versao400)
+                       {
+                           return new RecepcaoEventoManifestacaoDestinatario4AN(url, certificado, cfg.TimeOut);
+                       }
 
-                        return new RecepcaoEvento(url, certificado, cfg.TimeOut);
-                    }
+                       return new RecepcaoEvento(url, certificado, cfg.TimeOut);
+                   }
 
-                case ServicoNFe.RecepcaoEventoEpec:
-                    if (cfg.VersaoRecepcaoEventoEpec == VersaoServico.Versao400)
-                    {
-                        return new RecepcaoEvento4AN(url, certificado, cfg.TimeOut);
-                    }
+               case ServicoNFe.RecepcaoEventoEpec:
+                   if (cfg.VersaoRecepcaoEventoEpec == VersaoServico.Versao400)
+                   {
+                       return new RecepcaoEvento4AN(url, certificado, cfg.TimeOut);
+                   }
 
-                    return new RecepcaoEPEC(url, certificado, cfg.TimeOut);
-                    */
+                   return new RecepcaoEPEC(url, certificado, cfg.TimeOut);
+                   */
                 case ServicoNFe.NfeConsultaCadastro:
-                    
-                    if(cfg.cUF == Estado.CE)
+
+                    if (cfg.cUF == Estado.CE)
                     {
                         return new Wsdl.ConsultaCadastro.CE.CadConsultaCadastro2(url, certificado, cfg.TimeOut);
                     }
@@ -265,15 +260,15 @@ namespace NFe.Servicos
                     }
 
                     return new Wsdl.ConsultaCadastro.DEMAIS_UFs.CadConsultaCadastro2(url, certificado, cfg.TimeOut);
-                    /*
+
                 case ServicoNFe.NfeDownloadNF:
                     return new NfeDownloadNF(url, certificado, cfg.TimeOut);
-                    */
+
                 case ServicoNFe.NfceAdministracaoCSC:
                     return new NfceCsc(url, certificado, cfg.TimeOut);
-                    /*
+
                 case ServicoNFe.NFeDistribuicaoDFe:
-                    return new NfeDistDFeInteresse(url, certificado, cfg.TimeOut);*/
+                    return new NfeDistDFeInteresse(url, certificado, cfg.TimeOut);
             }
 
             return null;
