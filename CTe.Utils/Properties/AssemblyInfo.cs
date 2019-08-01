37673a07c5a1fd77ebb5d62003822496896e6176
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2019-08-01 16:40:30 -0300

    alterado versão assemblys

--- CTe.Utils/Properties/AssemblyInfo.cs
+++ CTe.Utils/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.0.750")]
-[assembly: AssemblyVersion("1.0.0.750")]
-[assembly: AssemblyFileVersion("1.0.0.750")]
+// [assembly: AssemblyVersion("1.0.0.751")]
+[assembly: AssemblyVersion("1.0.0.751")]
+[assembly: AssemblyFileVersion("1.0.0.751")]
