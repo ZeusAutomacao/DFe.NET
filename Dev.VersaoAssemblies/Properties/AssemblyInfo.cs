3faa41a2c8b885fef112b74539c33a2cb8eb466d
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2019-08-02 09:53:31 -0300

    alterado versão dos assemblys

--- Dev.VersaoAssemblies/Properties/AssemblyInfo.cs
+++ Dev.VersaoAssemblies/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.751")]
-[assembly: AssemblyFileVersion("1.0.0.751")]
+[assembly: AssemblyVersion("1.0.0.752")]
+[assembly: AssemblyFileVersion("1.0.0.752")]
