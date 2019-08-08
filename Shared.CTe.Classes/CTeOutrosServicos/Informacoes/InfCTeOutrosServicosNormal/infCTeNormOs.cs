7af51a616623329e55144fc135247e77a410c55c
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2019-08-08 09:07:54 -0300

    adicionado classes para emissão de CT-e OS

---
+++ Shared.CTe.Classes/CTeOutrosServicos/Informacoes/InfCTeOutrosServicosNormal/infCTeNormOs.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+using CTe.Classes.Informacoes;
+using CTe.Classes.Informacoes.infCteAnu;
+using CTe.Classes.Informacoes.infCteComp;
+
+namespace DFe.DocumentosEletronicos.CTe.CTeOS.Informacoes.InfCTeNormal
+{
+    public class infCTeNormOs
+    {
+        public infServico infServico { get; set; }
+
+        [XmlElement("infDocRef")]
+        public List<infDocRef> infDocRef { get; set; }
+
+        [XmlElement("seg")]
+        public List<segOs> seg { get; set; }
+
+        [XmlElement("infModal")]
+        public infModalOs infModal { get; set; }
+
+        [XmlElement("infCteSub")]
+        public infCteSubOs infCteSub { get; set; }
+
+        public infCteComp infCteComp { get; set; }
+
+        public infCteAnu infCteAnu { get; set; }
+
+        public List<autXML> autXml { get; set; }
+    }
+}
