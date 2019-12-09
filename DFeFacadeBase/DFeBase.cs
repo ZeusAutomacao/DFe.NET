517cbc3ca56730288a955e935fe8e5efb640e2d1
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2019-12-09 13:49:59 -0300

    criando projetos base

---
+++ DFeFacadeBase/DFeBase.cs
@@ -0,0 +1,9 @@
+﻿using DFe.Classes.Entidades;
+
+namespace DFeFacadeBase
+{
+    public abstract class DFeBase
+    {
+        public abstract Estado ObterEstado();
+    }
+}
---
+++ NFeFacade/NFeConfig.cs
@@ -0,0 +1,13 @@
+﻿using DFe.Classes.Entidades;
+using DFeFacadeBase;
+
+namespace NFeFacade
+{
+    public class NFeConfig : DFeBase
+    {
+        public override Estado ObterEstado()
+        {
+            return Estado.GO;
+        }
+    }
+}
