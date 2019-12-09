97a8326ba2b7eccf5e176bac6c500ee107a02c4a
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2019-12-09 14:26:06 -0300

    Adicionando métodos em IWSFacade e bolando eles ainda

--- DFeFacadeBase/IWSFacade.cs
+++ DFeFacadeBase/IWSFacade.cs
@@ -3,5 +3,15 @@
     public interface IWSFacade
     {
         IConsultaStatusRetorno ConsultaStatus();
+        void EnviaLote();
+        void CorrigeNota(string chaveAcesso, string textoCorrecao, int sequencialEvento);
+        void CancelaNota(string chaveAcesso, string protocoloNota, string motivoCancelamento);
+        void ConsultaNota(string chaveAcesso);
+        void Inutilizacao(int anoInutilizacaoNumeracao, string cnpjEmitente, string serie, string numeroInicial,
+            string numeroFinal, string justificativa);
+        void ConsultaCadastro(string cnpj, DFeEstado estadoUf);
+        void ManifestacaoDestinatario(string chave, DFTipoEvento tipoEvento, string motivo, string cnpj);
+        void ConsultaDistribuicao();
+
     }
 }
