b15009e71c8faab9536644cabf20ae79d2f9ac7a
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2019-12-09 20:31:48 -0300

    criado retorno status

---
+++ DFeFacadeBase/ConsultaStatusRetorno.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DFeFacadeBase
+{
+    public class ConsultaStatusRetorno : IConsultaStatusRetorno
+    {
+        public ConsultaStatusRetorno(string versao, DFeAmbiente ambiente, string versaoAplicacao, string status, string motivo, DFeEstado uf, DateTimeOffset dataRecebimento, DateTimeOffset dataRetorno, string observacao, string tempoMedio, string xmlEnvio, string xmlRetorno)
+        {
+            Versao = versao;
+            Ambiente = ambiente;
+            VersaoAplicacao = versaoAplicacao;
+            Status = status;
+            Motivo = motivo;
+            Uf = uf;
+            DataRecebimento = dataRecebimento;
+            DataRetorno = dataRetorno;
+            Observacao = observacao;
+            TempoMedio = tempoMedio;
+            XmlEnvio = xmlEnvio;
+            XmlRetorno = xmlRetorno;
+        }
+
+        public string Versao { get; }
+        public DFeAmbiente Ambiente { get; }
+        public string VersaoAplicacao { get; }
+        public string Status { get; }
+        public string Motivo { get; }
+        public DFeEstado Uf { get; }
+        public DateTimeOffset DataRecebimento { get; }
+        public DateTimeOffset DataRetorno { get; }
+        public string Observacao { get; }
+        public string TempoMedio { get; }
+        public string XmlEnvio { get; }
+        public string XmlRetorno { get; }
+    }
+}
--- DFeFacadeBase/IConsultaStatusRetorno.cs
+++ DFeFacadeBase/IConsultaStatusRetorno.cs
@@ -1,7 +1,21 @@
-﻿namespace DFeFacadeBase
+﻿using System;
+
+namespace DFeFacadeBase
 {
     public interface IConsultaStatusRetorno
     {
-        
+        string Versao { get; }
+        DFeAmbiente Ambiente { get; }
+        string VersaoAplicacao { get; }
+        string Status { get; }
+        string Motivo { get; }
+        DFeEstado Uf { get; }
+        DateTimeOffset DataRecebimento { get; }
+        DateTimeOffset DataRetorno { get; }
+        string Observacao { get; }
+        string TempoMedio { get; }
+
+        string XmlEnvio { get; }
+        string XmlRetorno { get; }
     }
 }
