70c0998be49ee8a784325ba9dcb1d4ed1552a2a9
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2020-03-13 14:15:37 -0300

    incluido validação de schemas do novo evento de mdf-e

---
+++ MDFe.Classes/Extensoes/ExtevPagtoOperMDFe.cs
@@ -0,0 +1,28 @@
+﻿using DFe.Utils;
+using MDFe.Classes.Informacoes.Evento.CorpoEvento;
+using MDFe.Utils.Configuracoes;
+using MDFe.Utils.Flags;
+using MDFe.Utils.Validacao;
+
+namespace MDFe.Classes.Extencoes
+{
+    public static class ExtevPagtoOperMDFe
+    {
+        public static void ValidaSchema(this evPagtoOperMDFe evIncDFeMDFe)
+        {
+            var ev = evIncDFeMDFe.XmlString();
+
+            switch (MDFeConfiguracao.VersaoWebService.VersaoLayout)
+            {
+                case VersaoServico.Versao300:
+                    Validador.Valida(ev, "evPagtoOperMDFe_v3.00.xsd");
+                    break;
+            }
+        }
+
+        public static string XmlString(this evPagtoOperMDFe evIncDFeMDFe)
+        {
+            return FuncoesXml.ClasseParaXmlString(evIncDFeMDFe);
+        }
+    }
+}
