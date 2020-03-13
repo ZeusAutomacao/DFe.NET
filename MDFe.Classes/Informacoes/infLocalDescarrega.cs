718ecb7d1e1dacb0a794465fe06ce4b0de3ad1b3
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2020-03-13 10:20:22 -0300

    concluido prodPred, infLotacao, infLocalDescarrega, infLocalCarrega

---
+++ MDFe.Classes/Informacoes/infLocalDescarrega.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MDFe.Classes.Informacoes
+{
+    [Serializable]
+    public class infLocalDescarrega
+    {
+        public string CEP { get; set; }
+
+        public decimal latitude { get; set; }
+
+        public decimal Logintude { get; set; }
+    }
+}
