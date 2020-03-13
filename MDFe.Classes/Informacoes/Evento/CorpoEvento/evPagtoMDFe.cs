dcc73f6b26e9c14722ee6c0a2f427dab038a6bdc
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2020-03-13 11:00:04 -0300

    evento de pagamento mdf-e criado classes

---
+++ MDFe.Classes/Informacoes/Evento/CorpoEvento/evPagtoMDFe.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MDFe.Classes.Informacoes.Evento.CorpoEvento
+{
+    [Serializable]
+    public class evPagtoMDFe : MDFeEventoContainer
+    {
+        public evPagtoMDFe()
+        {
+            descEvento = "Pagamento Operação MDF-e";
+        }
+
+        public string descEvento { get; set; }
+
+        public string nProt { get; set; }
+
+        public infViagens infViagens { get; set; }
+
+        public List<infPag> infPag { get; set; }
+    }
+
+    [Serializable]
+    public class infViagens
+    {
+        public int qtdViagens { get; set; }
+
+        public int nroViagens { get; set; }
+    }
+}
