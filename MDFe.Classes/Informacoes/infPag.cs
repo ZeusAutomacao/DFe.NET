a67783c3e29322bd3c5c5600855102602c7b08a0
Marcos Gerene Felix Spirito <mgf.spirito@gmail.com>
2020-03-17 15:59:09 -0300

    [-] correções para NT2020/001

--- MDFe.Classes/Informacoes/infPag.cs
+++ MDFe.Classes/Informacoes/infPag.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DFe.Classes;
+using System;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 
 namespace MDFe.Classes.Informacoes
 {
@@ -11,11 +13,22 @@ namespace MDFe.Classes.Informacoes
         public string CNPJ { get; set; }
         public string idEstrangeiro { get; set; }
 
+        [XmlElement(ElementName = "Comp")]
         public List<Comp> Comp { get; set; }
 
+        [XmlIgnore]
         public decimal vContrato { get; set; }
+
+        [XmlElement("vContrato")]
+        public decimal vContratoProxy
+        {
+            get { return vContrato.Arredondar(2); }
+            set { vContrato = value.Arredondar(2); }
+        }
+
         public indPag indPag { get; set; }
 
+        [XmlElement(ElementName = "infPrazo")]
         public List<infPrazo> infPrazo { get; set; }
 
         public infBanc infBanc { get; set; }
