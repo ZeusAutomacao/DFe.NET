3ebaf9828066874a23270c82909498204a8f9f4a
Fabio Ciaf <fabio@ciaf.com.br>
2020-04-24 15:13:39 -0300

    Update ServicosNFe.cs

--- Shared.NFe.Servicos/ServicosNFe.cs
+++ Shared.NFe.Servicos/ServicosNFe.cs
@@ -558,7 +558,7 @@ namespace NFe.Servicos
             };
 
             if (_cFgServico.cUF == Estado.MT || _cFgServico.RemoverAcentos){
-                descEvento = "Carta de Correcao";
+                detEvento.descEvento = "Carta de Correcao";
                 detEvento.xCondUso =
                     "A Carta de Correcao e disciplinada pelo paragrafo 1o-A do art. 7o do Convenio S/N, de 15 de dezembro de 1970 e pode ser utilizada para regularizacao de erro ocorrido na emissao de documento fiscal, desde que o erro nao esteja relacionado com: I - as variaveis que determinam o valor do imposto tais como: base de calculo, aliquota, diferenca de preco, quantidade, valor da operacao ou da prestacao; II - a correcao de dados cadastrais que implique mudanca do remetente ou do destinatario; III - a data de emissao ou de saida.";
             }
