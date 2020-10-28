55d2a8b975792255b76fd445e3cd3acf4d5b75c8
Marcos Gerene Felix Spirito <mgf.spirito@gmail.com>
2020-10-28 18:06:01 -0300

    [-] Corrigindo tipo dos campos pesoL, pesoB e cStat https://github.com/ZeusAutomacao/DFe.NET/issues/1187

--- Shared.NFe.Classes/Informacoes/Detalhe/ProdEspecifico/veicProd.cs
+++ Shared.NFe.Classes/Informacoes/Detalhe/ProdEspecifico/veicProd.cs
@@ -34,10 +34,6 @@ namespace NFe.Classes.Informacoes.Detalhe.ProdEspecifico
 {
     public class veicProd : ProdutoEspecifico
     {
-        private decimal _pesoL;
-        private decimal _pesoB;
-        private decimal _cmt;
-
         /// <summary>
         ///     J02 - Tipo da operação
         /// </summary>
@@ -72,20 +68,12 @@ namespace NFe.Classes.Informacoes.Detalhe.ProdEspecifico
         /// <summary>
         ///     J08 - Peso Líquido
         /// </summary>
-        public decimal pesoL
-        {
-            get { return _pesoL; }
-            set { _pesoL = value.Arredondar(3); }
-        }
+        public string pesoL { get; set; }
 
         /// <summary>
         ///     J09 - Peso Bruto
         /// </summary>
-        public decimal pesoB
-        {
-            get { return _pesoB; }
-            set { _pesoB = value.Arredondar(3); }
-        }
+        public decimal pesoB { get; set; }
 
         /// <summary>
         ///     J10 - Serial (série)
