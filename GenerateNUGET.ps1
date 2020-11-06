08d333b94d337b5c463de72dc5e6c2e0c20ec7bc
rafael-habil <rafael@habiltecnologia.com.br>
2020-11-06 14:14:55 -0300

    correção de script e versionamento
    correção de script e versionamen to

--- GenerateNUGET.ps1
+++ GenerateNUGET.ps1
@@ -1,11 +1,11 @@
-cd C:\Habil\Repo\DFe.NET
-iex "& { $(irm https://aka.ms/install-artifacts-credprovider.ps1) } -AddNetfx"
-New-Alias -Name vswhere -Value "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
+param(
+[Switch]$skipBuild,
+[Switch]$skipPublish)
 function GetPackageName {
     param (
         $nugetMessage
     )
-
+	
     $nugetMessage = $nugetMessage.Split([Environment]::NewLine) | Select -Last 1
     $nugetMessage = $nugetMessage.substring($nugetMessage.IndexOf("'")+1)
     $packageName =  $nugetMessage.remove($nugetMessage.length-2,2) 
@@ -15,18 +15,54 @@ function GetPackageName {
     Write-Output $packageName 
 }
 
-$MSBuildPath = vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath
-$MSBuildPath = $MSBuildPath + "\MSBuild\Current\Bin\MsBuild.exe"
-&$MSBuildPath "Zeus NFe.sln" /t:"Clean;Rebuild" -property:Configuration=Release
-nuget sources add -Name "HabilTecnologia" -Source "https://pkgs.dev.azure.com/HabilTecnologia/_packaging/HabilTecnologia/nuget/v3/index.json"
-$packageName = GetPackageName($(nuget pack Nuget\nfenfcedll\Zeus.Net.NFe.NFCe.nuspec -Symbols -SymbolPackageFormat snupkg))
-dotnet nuget push --source "HabilTecnologia" --api-key az $packageName --interactive
-$packageName = GetPackageName($(nuget pack NuGet\ctedll\Zeus.Net.CTe.nuspec -Symbols -SymbolPackageFormat snupkg))
-dotnet nuget push --source "HabilTecnologia" --api-key az $packageName --interactive
-$packageName = GetPackageName($(nuget pack NuGet\mdfedll\Zeus.Net.MDFe.nuspec -Symbols -SymbolPackageFormat snupkg))
-dotnet nuget push --source "HabilTecnologia" --api-key az $packageName --interactive
+function PublishNugetIfNotSkipped {
+    param (
+        $packageName		
+    )
+    if($skipPublish -eq $False){
+      dotnet nuget push --source "HabilTecnologia" --api-key az $packageName --interactive
+    }
+	else{
+		Write-Host 'A publicacao para o nuget do pacote '$packageName' foi desabilitada pelo parametro -skipPublish';
+	}
+}
+if($skipBuild -eq $False){
+   Write-Host 'Fazendo o build da solution....';
+   New-Alias -Name vswhere -Value "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
+   $MSBuildPath = vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath
+   $MSBuildPath = $MSBuildPath + "\MSBuild\Current\Bin\MsBuild.exe"
+   &$MSBuildPath "Zeus NFe.sln" /t:"Clean;Rebuild" -property:"Configuration=Release;IncludeSymbols=true" /noconsolelogger /nologo
+}
+else{
+  Write-Host 'O build da solution foi desabilitado pelo parametro --skipBuild';		
+}
+if($skipPublish -eq $False){
+  Write-Host 'Adicionando autenticacao do feed do nuget...';
+  iex "& { $(irm https://aka.ms/install-artifacts-credprovider.ps1) } -AddNetfx"
+  # nuget sources add -Name "HabilTecnologia" -Source "https://pkgs.dev.azure.com/HabilTecnologia/_packaging/HabilTecnologia/nuget/v3/index.json"
+  $returnMsg = dotnet nuget add source "https://pkgs.dev.azure.com/HabilTecnologia/_packaging/HabilTecnologia/nuget/v3/index.json" --name "HabilTecnologia"
+  if($rMsg.GetType().Name -eq 'Object[]'){
+    Write-Host $rMsg[0]
+  }
+  else {
+    Write-Host $rMsg
+  }
+   
+}
 
-Write-Host -NoNewLine 'Press any key to continue...';
+Write-Host 'Gerando os pacotes do nuget....';
+
+$packageName = GetPackageName($(nuget pack Nuget\nfenfcedll\Zeus.Net.NFe.NFCe.nuspec -Symbols -SymbolPackageFormat snupkg))
+PublishNugetIfNotSkipped($packageName)
+
+$packageName = GetPackageName($(nuget pack NuGet\ctedll\Zeus.Net.CTe.nuspec -Symbols -SymbolPackageFormat snupkg))
+PublishNugetIfNotSkipped($packageName)
+
+$packageName = GetPackageName($(nuget pack NuGet\mdfedll\Zeus.Net.MDFe.nuspec -Symbols -SymbolPackageFormat snupkg))
+PublishNugetIfNotSkipped($packageName)
+
+Write-Host 'Os pacotes nupkg gerados estarao na raiz do diretorio da solution.';
+Write-Host 'Processo concluído, aperte qualquer tecla para fechar.';
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 
 
