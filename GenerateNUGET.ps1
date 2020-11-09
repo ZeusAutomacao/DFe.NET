4a4f4067ab85416b1752c7b6b2914ae4d026342b
rafael-habil <rafael@habiltecnologia.com.br>
2020-11-09 10:36:18 -0300

    Atualização do script de geração de nuget
    Atualização do script de geração de nuget e atualização do gitnore para não enviar .snupkg

--- GenerateNUGET.ps1
+++ GenerateNUGET.ps1
@@ -1,6 +1,8 @@
 param(
 [Switch]$skipBuild,
 [Switch]$skipPublish)
+
+#declaracao de funcoes
 function GetPackageName {
     param (
         $nugetMessage
@@ -15,54 +17,85 @@ function GetPackageName {
     Write-Output $packageName 
 }
 
+function ExecNugetPack {
+    param (
+        $nuspecPath
+    )
+	
+	$returnMsg = $(nuget pack $nuspecPath -Symbols -OutputFileNamesWithoutVersion -SymbolPackageFormat snupkg)
+  
+	$packageName = GetPackageName($returnMsg)
+	 
+	 Write-Host 'Pacote ' $packageName ' gerado com sucesso.';
+	
+	
+	
+    Write-Output  $packageName
+}
+
 function PublishNugetIfNotSkipped {
     param (
         $packageName		
     )
     if($skipPublish -eq $False){
-      dotnet nuget push --source "HabilTecnologia" --api-key az $packageName --interactive
+	
+      Write-Host '';
+	  Write-Host 'Enviando pacote ' $packageName ' para o nuget'
+	  Write-Host '#######################################################################'
+      dotnet nuget push --source "HabilTecnologia" --api-key az $packageName --interactive --skip-duplicate
+	  Write-Host '#######################################################################'
+	  Write-Host ''
     }
 	else{
 		Write-Host 'A publicacao para o nuget do pacote '$packageName' foi desabilitada pelo parametro -skipPublish';
+		Write-Host '';
 	}
 }
-if($skipBuild -eq $False){
-   Write-Host 'Fazendo o build da solution....';
-   New-Alias -Name vswhere -Value "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
-   $MSBuildPath = vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath
-   $MSBuildPath = $MSBuildPath + "\MSBuild\Current\Bin\MsBuild.exe"
-   &$MSBuildPath "Zeus NFe.sln" /t:"Clean;Rebuild" -property:"Configuration=Release;IncludeSymbols=true" /noconsolelogger /nologo
-}
-else{
-  Write-Host 'O build da solution foi desabilitado pelo parametro --skipBuild';		
-}
-if($skipPublish -eq $False){
-  Write-Host 'Adicionando autenticacao do feed do nuget...';
-  iex "& { $(irm https://aka.ms/install-artifacts-credprovider.ps1) } -AddNetfx"
-  # nuget sources add -Name "HabilTecnologia" -Source "https://pkgs.dev.azure.com/HabilTecnologia/_packaging/HabilTecnologia/nuget/v3/index.json"
-  $returnMsg = dotnet nuget add source "https://pkgs.dev.azure.com/HabilTecnologia/_packaging/HabilTecnologia/nuget/v3/index.json" --name "HabilTecnologia"
-  if($rMsg.GetType().Name -eq 'Object[]'){
-    Write-Host $rMsg[0]
-  }
-  else {
-    Write-Host $rMsg
-  }
+
+#inico do script
+ Write-Host '';
+ if($skipBuild -eq $False){   
+    Write-Host 'Fazendo o build da solution....';
+    New-Alias -Name vswhere -Value "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
+    $MSBuildPath = vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath
+    $MSBuildPath = $MSBuildPath + "\MSBuild\Current\Bin\MsBuild.exe"
+    &$MSBuildPath "Zeus NFe.sln" /t:"Clean;Rebuild" -property:"Configuration=Release;IncludeSymbols=true" /noconsolelogger /nologo
+ }
+ else{
+   Write-Host 'O build da solution foi desabilitado pelo parametro --skipBuild';		
+ }
+ Write-Host '';
+ 
+ if($skipPublish -eq $False){
+   Write-Host 'Adicionando autenticacao do feed do nuget...';
+   Write-Host '#######################################################################'
+   iex "& { $(irm https://aka.ms/install-artifacts-credprovider.ps1) } -AddNetfx"
+   $returnMsg = $(dotnet nuget add source "https://pkgs.dev.azure.com/HabilTecnologia/_packaging/HabilTecnologia/nuget/v3/index.json" --name "HabilTecnologia")
+   if($returnMsg.GetType().Name -eq 'Object[]'){
+     Write-Host $returnMsg[0]
+   }
+   else {
+     Write-Host $returnMsg
+   }
+   Write-Host '#######################################################################'
    
-}
+ }
 
-Write-Host 'Gerando os pacotes do nuget....';
+ Write-Host '';
+ Write-Host 'Gerando os pacotes do nuget....';
 
-$packageName = GetPackageName($(nuget pack Nuget\nfenfcedll\Zeus.Net.NFe.NFCe.nuspec -Symbols -SymbolPackageFormat snupkg))
-PublishNugetIfNotSkipped($packageName)
+ $packageName = ExecNugetPack("Nuget\nfenfcedll\Zeus.Net.NFe.NFCe.nuspec")
+ PublishNugetIfNotSkipped($packageName)
 
-$packageName = GetPackageName($(nuget pack NuGet\ctedll\Zeus.Net.CTe.nuspec -Symbols -SymbolPackageFormat snupkg))
-PublishNugetIfNotSkipped($packageName)
+ $packageName = ExecNugetPack("NuGet\ctedll\Zeus.Net.CTe.nuspec")
+ PublishNugetIfNotSkipped($packageName)
 
-$packageName = GetPackageName($(nuget pack NuGet\mdfedll\Zeus.Net.MDFe.nuspec -Symbols -SymbolPackageFormat snupkg))
-PublishNugetIfNotSkipped($packageName)
+ $packageName = ExecNugetPack("NuGet\mdfedll\Zeus.Net.MDFe.nuspec")
+ PublishNugetIfNotSkipped($packageName)
 
-Write-Host 'Os pacotes nupkg gerados estarao na raiz do diretorio da solution.';
-Write-Host 'Processo concluído, aperte qualquer tecla para fechar.';
+ Write-Host '';
+ Write-Host 'Os pacotes nupkg gerados estarao na raiz do diretorio da solution.';
+ Write-Host 'Processo concluido, aperte qualquer tecla para fechar.';
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 
 
