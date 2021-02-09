ca1fb968edae9e5ba55144be8b6dccd42a9cbc62
Pedro Henrique Perim Franco Neiva <pedro.neiva@hubconexa.com>
2021-02-09 20:03:48 -0300

    Adicionado Campos referente ao intermediador

---
+++ Shared.NFe.Classes/Informacoes/Intermediador/infIntermed.cs
@@ -0,0 +1,15 @@
+﻿namespace Shared.NFe.Classes.Informacoes.Intermediador
+{
+    public class infIntermed
+    {
+        /// <summary>
+        ///     YB02 - CNPJ do Intermediador da Transação (agenciador, plataforma de delivery, marketplace e similar) de serviços e de negócios.
+        /// </summary>
+        public string CNPJ { get; set; }
+
+        /// <summary>
+        ///     YB03 - Identificador cadastrado no intermediador
+        /// </summary>
+        public string idCadIntTran { get; set; }
+    }
+}
