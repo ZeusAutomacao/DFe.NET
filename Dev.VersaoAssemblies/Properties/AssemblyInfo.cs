91c591458fc451cb8db8d8ad1cd6f6f2baba7305
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-02-11 16:26:47 -0300

    alterado versão para lançamento de versão no nuget

--- Dev.VersaoAssemblies/Properties/AssemblyInfo.cs
+++ Dev.VersaoAssemblies/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.770")]
-[assembly: AssemblyFileVersion("1.0.0.770")]
+[assembly: AssemblyVersion("1.0.0.772")]
+[assembly: AssemblyFileVersion("1.0.0.772")]
