de3757484c561d05b1f2f5f92bebe57c88b34e0c
adriano_g3 <adriano@g3soft.com.br>
2021-04-05 14:14:48 -0300

    Ambiente Homologação em 03/05/2021. Descrição do meio de pagamento. Preencher informando o meio de pagamento utilizado quando o código do meio de pagamento for informado como 99-outros.

--- Shared.NFe.Classes/Informacoes/Pagamento/detPag.cs
+++ Shared.NFe.Classes/Informacoes/Pagamento/detPag.cs
@@ -6,24 +6,35 @@ namespace NFe.Classes.Informacoes.Pagamento
     {
         private decimal _vPag;
 
-
+        /// <summary>
+        ///     YA01b - Indicador da Forma de Pagamento
+        /// </summary>
         public IndicadorPagamentoDetalhePagamento? indPag { get; set; }
 
         public bool indPagSpecified { get { return indPag.HasValue; } }
 
         /// <summary>
-        /// YA02 - Forma de pagamento
+        ///     YA02 - Meio de pagamento
         /// </summary>
         public FormaPagamento tPag { get; set; }
 
+        /// <summary>
+        ///     YA02a - Descrição do Meio de Pagamento
+        /// </summary>
+        public string xPag { get; set; }
+
+        /// <summary>
+        ///     YA03 - Valor do Pagamento
+        /// </summary>
         public decimal vPag
         {
             get { return _vPag.Arredondar(2); }
             set { _vPag = value.Arredondar(2); }
         }
 
+        /// <summary>
+        ///     YA04 - Grupo de Cartões
+        /// </summary>
         public card card { get; set; }
-
-
     }
 }
