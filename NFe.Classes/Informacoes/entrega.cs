1bb57ab3c51cb4120728653b7cef805b09842c95
Bruno Alencar <bruno.idealencar@gmail.com>
2021-04-27 13:33:55 -0300

    Add delivery and withdrawal fields

--- NFe.Classes/Informacoes/entrega.cs
+++ NFe.Classes/Informacoes/entrega.cs
@@ -31,6 +31,7 @@
 /* Rua Comendador Francisco josé da Cunha, 111 - Itabaiana - SE - 49500-000     */
 /********************************************************************************/
 using System;
+using System.Xml.Serialization;
 
 namespace NFe.Classes.Informacoes
 {
@@ -77,6 +78,11 @@ namespace NFe.Classes.Informacoes
         }
 
         /// <summary>
+        /// Razão Social ou nome do recebedor
+        /// </summary>
+        public string xNome { get; set; }
+
+        /// <summary>
         ///     G03 - Logradouro
         /// </summary>
         public string xLgr { get; set; }
@@ -111,5 +117,96 @@ namespace NFe.Classes.Informacoes
         ///     G09 - Sigla da UF, informar EX para operações com o exterior.
         /// </summary>
         public string UF { get; set; }
+
+        /// <summary>
+        ///     E13 - Código do CEP
+        ///     <para>Informar os zeros não significativos. (NT 2011/004)</para>
+        /// </summary>
+        [XmlIgnore]
+        public long? CEP { get; set; }
+
+        [XmlElement(ElementName = "CEP")]
+        public string ProxyCEP
+        {
+            get
+            {
+                return CEP?.ToString("D8");
+            }
+            set { CEP = long.Parse(value); }
+        }
+
+        /// <summary>
+        ///     E14 - Código do País
+        ///     <para>1058 - Brasil</para>
+        /// </summary>
+        public int? cPais { get; set; }
+
+        /// <summary>
+        ///     E15 - Nome do País
+        ///     <para>Brasil ou BRASIL</para>
+        /// </summary>
+        public string xPais { get; set; }
+
+        public bool cPaisSpecified { get { return cPais.HasValue; } }
+
+        /// <summary>
+        ///     E16 - Telefone
+        ///     <para>
+        ///         Preencher com o Código DDD + número do telefone. Nas operações com exterior é permitido informar o código do
+        ///         país + código da localidade + número do telefone (v.2.0)
+        ///     </para>
+        /// </summary>
+        public long? fone { get; set; }
+
+        /// <summary>
+        ///     E19 - Informar o e-mail do destinatário. O campo pode ser utilizado para informar o e-mail de recepção da NF-e
+        ///     indicada pelo destinatário
+        /// </summary>
+        public string email { get; set; }
+
+        /// <summary>
+        ///     E17 - Inscrição Estadual
+        ///     <para>Campo de informação obrigatória nos casos de emissão própria (procEmi = 0, 2 ou 3).</para>
+        ///     <para>
+        ///         A IE deve ser informada apenas com algarismos para destinatários contribuintes do ICMS, sem caracteres de
+        ///         formatação (ponto, barra, hífen, etc.);
+        ///     </para>
+        ///     <para>
+        ///         O literal “ISENTO” deve ser informado apenas para contribuintes do ICMS que são isentos de inscrição no
+        ///         cadastro de contribuintes do ICMS e estejam emitindo NF-e avulsa;
+        ///     </para>
+        /// </summary>
+        public string IE { get; set; }
+
+
+        public bool ShouldSerializecPais()
+        {
+            return cPais.HasValue;
+        }
+
+        public bool ShouldSerializexPais()
+        {
+            return !string.IsNullOrEmpty(xPais);
+        }
+
+        public bool ShouldSerializefone()
+        {
+            return fone.HasValue;
+        }
+
+        public bool ShouldSerializexCpl()
+        {
+            return !string.IsNullOrEmpty(xCpl);
+        }
+
+        public bool ShouldSerializexNome()
+        {
+            return !string.IsNullOrEmpty(xNome);
+        }
+
+        public bool ShouldSerializeCEP()
+        {
+            return CEP.HasValue;
+        }
     }
 }
