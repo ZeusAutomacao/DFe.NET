79f600ac88c059255953b9eb7cb1a6846833307f
Marcos Gerene F. Spirito <mgf.spirito@gmail.com>
2021-05-27 10:49:41 -0300

    [Danfe] Impressão do Danfe Simplificado

---
+++ NFe.Danfe.Fast/NFe/DanfeFrSimplificado.cs
@@ -0,0 +1,21 @@
+﻿using NFe.Classes;
+using NFe.Danfe.Base.NFe;
+using Shared.DFe.Danfe.Fast;
+
+namespace NFe.Danfe.Fast.NFe
+{
+    public class DanfeFrSimplificado : DanfeBase
+    {
+        /// <summary>
+        /// Construtor da classe responsável pela impressão do DANFE da NFe em Fast Reports
+        /// </summary>
+        /// <param name="proc">Objeto do tipo nfeProc</param>
+        /// <param name="configuracaoDanfeNfe">Objeto do tipo <see cref="ConfiguracaoDanfeNfe"/> contendo as definições de impressão</param>
+        /// <param name="desenvolvedor">Texto do desenvolvedor a ser informado no DANFE</param>
+        public DanfeFrSimplificado(nfeProc proc, ConfiguracaoDanfeNfe configuracaoDanfeNfe, string desenvolvedor = "", string arquivoRelatorio = "")
+        {
+            Relatorio = DanfeSharedHelper.GenerateDanfeFrNfeReport(proc, configuracaoDanfeNfe, Properties.Resources.NFeSimplificado, desenvolvedor, arquivoRelatorio);
+        }
+
+    }
+}
