ff55e379e1ab7a7b4b572c564c11014fd4d8ae43
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-06-13 13:47:13 -0300

    Convertido consulta protocolo

--- Shared.DFe.Wsdl/Common/SoapUrls.cs
+++ Shared.DFe.Wsdl/Common/SoapUrls.cs
@@ -31,6 +31,8 @@ namespace CTe.CTeOSDocumento.Soap
                     return "http://www.portalfiscal.inf.br/mdfe/wsdl/MDFeRetRecepcao";
                 case TipoEvento.MDFeNaoEncerrado:
                     return "http://www.portalfiscal.inf.br/mdfe/wsdl/MDFeConsNaoEnc";
+                case TipoEvento.MDFeConsulta:
+                    return "http://www.portalfiscal.inf.br/mdfe/wsdl/MDFeConsulta";
                 default:
                     throw new ArgumentOutOfRangeException(nameof(tipoEvento), tipoEvento, null);
             }
