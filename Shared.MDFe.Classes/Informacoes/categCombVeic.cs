8e6ba32e250fd73a96f19ef4f25e0dcd4edf1bbb
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-07-27 15:27:49 -0300

    Correção da NT MDFe_Nota_Tecnica_2021_001_v1.02.pdf
    Somente a parte de vale pedágio

--- Shared.MDFe.Classes/Informacoes/categCombVeic.cs
+++ Shared.MDFe.Classes/Informacoes/categCombVeic.cs
@@ -7,28 +7,28 @@ namespace MDFe.Classes.Informacoes
         [XmlEnum("02")]
         VeiculoComercial2Eixos = 02,
 
-        [XmlEnum("03")]
+        [XmlEnum("04")]
         VeiculoComercial3Eixos = 03,
 
-        [XmlEnum("04")]
+        [XmlEnum("06")]
         VeiculoComercial4Eixos = 04,
 
-        [XmlEnum("05")]
+        [XmlEnum("07")]
         VeiculoComercial5Eixos = 05,
 
-        [XmlEnum("06")]
+        [XmlEnum("08")]
         VeiculoComercial6Eixos = 06,
 
-        [XmlEnum("07")]
+        [XmlEnum("10")]
         VeiculoComercial7Eixos = 07,
 
-        [XmlEnum("08")]
+        [XmlEnum("11")]
         VeiculoComercial8Eixos = 08,
 
-        [XmlEnum("09")]
+        [XmlEnum("12")]
         VeiculoComercial9Eixos = 09,
 
-        [XmlEnum("10")]
+        [XmlEnum("13")]
         VeiculoComercial10Eixos = 10,
     }
 }
