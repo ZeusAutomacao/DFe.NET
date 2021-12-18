a8bb860e9cce7630e101bdb64a7e3ab4cb62d300
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-12-18 17:52:29 -0300

    Modificando aos poucos a métodologia de setagem de Urls no Enderecador , Motivo é poder cada um alterar as urls a gosto
    UF: Ceará anunciou que vai trocar as urls de NF-e cada um pode criar uma maneira no seu sistema para entrar em vigor as novas URLs
    
    O método padrão do Zeus será implementado por data é hora o que no meu ver não é ideal mas por hora é assim.
    
    Por isso surgiu a ideia, de cada um poder implementar a gosto de suas regras de sistema.
    
    Ao terminar de implementar irei Abrir uma Issue com mais detalhes de como cada um poderá implementar a gosto se por acaso não quiser usar o Padrão do Zeus é estou aberto a Ideias para o Padrão do Zeus também.

---
+++ Shared.NFe.Utils/ConfiguracaoUrls.cs
@@ -0,0 +1,14 @@
+﻿using NFe.Utils.Enderecos;
+
+namespace NFe.Utils
+{
+    public static class ConfiguracaoUrls
+    {
+        static ConfiguracaoUrls()
+        {
+            FactoryUrl = FactoryUrl.CriaFactoryUrl();
+        }
+
+        public static FactoryUrl FactoryUrl { get; set; }
+    }
+}
