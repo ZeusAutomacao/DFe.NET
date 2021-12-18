767d079a8ef47267140d6a7d286f8cac7c14c81f
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-12-18 18:49:08 -0300

    Ajustes para setagem de url , recarregamento de endereços

--- Shared.NFe.Utils/ConfiguracaoUrls.cs
+++ Shared.NFe.Utils/ConfiguracaoUrls.cs
@@ -4,11 +4,21 @@ namespace NFe.Utils
 {
     public static class ConfiguracaoUrls
     {
+        private static FactoryUrl _factoryUrl;
+
         static ConfiguracaoUrls()
         {
             FactoryUrl = FactoryUrl.CriaFactoryUrl();
         }
 
-        public static FactoryUrl FactoryUrl { get; set; }
+        public static FactoryUrl FactoryUrl
+        {
+            get { return _factoryUrl; }
+            set
+            {
+                _factoryUrl = value;
+                Enderecador.CarregarEnderecos();
+            }
+        }
     }
 }
