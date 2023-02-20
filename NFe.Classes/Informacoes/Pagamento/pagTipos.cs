0f032d14c33b6fc9108b3fabcd60fbca749c56c4
rhfranzoni <74259528+rhfranzoni@users.noreply.github.com>
2023-02-20 10:53:17 -0300

    Update pagTipos.cs

--- NFe.Classes/Informacoes/Pagamento/pagTipos.cs
+++ NFe.Classes/Informacoes/Pagamento/pagTipos.cs
@@ -107,38 +107,93 @@ namespace NFe.Classes.Informacoes.Pagamento
     }
 
     /// <summary>
-    ///     <para>01=Visa</para>
-    ///     <para>02=Mastercard</para>
-    ///     <para>03=American Express</para>
-    ///     <para>04=Sorocred</para>
-    ///     <para>05=Diners Club (versão 4.00)</para>
-    ///     <para>06=Elo (versão 4.00)</para>
-    ///     <para>07=Hipercard (versão 4.00)</para>
-    ///     <para>08=Aura (versão 4.00)</para>
-    ///     <para>09=Cabal (versão 4.00)</para>
-    ///     <para>99=Outros</para>
+    ///     Bandeira da operadora de cartão de crédito e/ou débito
+    ///     <para>01 - Visa</para>
+    ///     <para>02 - Mastercard</para>
+    ///     <para>03 - American Express</para>
+    ///     <para>04 - Sorocred</para>
+    ///     <para>05 - Diners Club (versão 4.00)</para>
+    ///     <para>06 - Elo (versão 4.00)</para>
+    ///     <para>07 - Hipercard (versão 4.00)</para>
+    ///     <para>08 - Aura (versão 4.00)</para>
+    ///     <para>09 - Cabal (versão 4.00)</para>
+    ///     <para>10 - Alelo</para>
+    ///     <para>11 - Banes Card</para>
+    ///     <para>12 - CalCard</para>
+    ///     <para>13 - Credz</para>
+    ///     <para>14 - Discover</para>
+    ///     <para>15 - GoodCard</para>
+    ///     <para>16 - GreenCard</para>
+    ///     <para>17 - Hiper</para>
+    ///     <para>18 - JCB</para>
+    ///     <para>19 - Mais</para>
+    ///     <para>20 - MaxVan</para>
+    ///     <para>21 - Policard</para>
+    ///     <para>22 - RedeCompras</para>
+    ///     <para>23 - Sodexo</para>
+    ///     <para>24 - ValeCard</para>
+    ///     <para>25 - Verocheque</para>
+    ///     <para>26 - VR</para>
+    ///     <para>27 - Ticket</para>
+    ///     <para>99 - Outros</para>
     /// </summary>
     public enum BandeiraCartao
     {
-        [Description("Visa")] [XmlEnum("01")] bcVisa,
+        [Description("Visa")] [XmlEnum("01")] bcVisa = 01,
 
-        [Description("Mastercard")] [XmlEnum("02")] bcMasterCard,
+        [Description("Mastercard")] [XmlEnum("02")] bcMasterCard = 02,
 
-        [Description("American Express")] [XmlEnum("03")] bcAmericanExpress,
+        [Description("American Express")] [XmlEnum("03")] bcAmericanExpress = 03,
 
-        [Description("Sorocred")] [XmlEnum("04")] bcSorocred,
+        [Description("Sorocred")] [XmlEnum("04")] bcSorocred = 04,
 
-        [Description("Diners Club")] [XmlEnum("05")] bcDinersClub,
+        [Description("Diners Club")] [XmlEnum("05")] bcDinersClub = 05,
 
-        [Description("Elo")] [XmlEnum("06")] Elo,
+        [Description("Elo")] [XmlEnum("06")] Elo = 06,
 
-        [Description("Hipercard")] [XmlEnum("07")] Hipercard,
+        [Description("Hipercard")] [XmlEnum("07")] Hipercard = 07,
 
-        [Description("Aura")] [XmlEnum("08")] Aura,
+        [Description("Aura")] [XmlEnum("08")] Aura = 08,
 
-        [Description("Cabal")] [XmlEnum("09")] Cabal,
+        [Description("Cabal")] [XmlEnum("09")] Cabal = 09,
 
-        [Description("Outros")] [XmlEnum("99")] bcOutros
+        [Description("Alelo")]  [XmlEnum("10")] Alelo = 10,
+
+        [Description("BanesCard")] [XmlEnum("11")] BanesCard = 11,
+
+        [Description("CalCard")] [XmlEnum("12")] CalCard = 12,
+
+        [Description("Credz")] [XmlEnum("13")] Credz = 13,
+
+        [Description("Discover")] [XmlEnum("14")] Discover = 14,
+
+        [Description("GoodCard")] [XmlEnum("15")] GoodCard = 15,
+
+        [Description("GreenCard")] [XmlEnum("16")] GreenCard = 16,
+
+        [Description("Hiper")] [XmlEnum("17")] Hiper = 17,
+
+        [Description("JCB")] [XmlEnum("18")] JCB = 18,
+
+        [Description("Mais")] [XmlEnum("19")] Mais = 19,
+
+        [Description("MaxVan")] [XmlEnum("20")] MaxVan = 20,
+
+        [Description("Policard")] [XmlEnum("21")] Policard = 21,
+
+        [Description("RedeCompras")] [XmlEnum("22")] RedeCompras = 22,
+
+        [Description("Sodexo")] [XmlEnum("23")] Sodexo = 23,
+
+        [Description("ValeCard")] [XmlEnum("24")] ValeCard = 24,
+
+        [Description("Verocheque")] [XmlEnum("25")] Verocheque = 25,
+
+        [Description("VR")] [XmlEnum("26")] VR = 26,
+
+        [Description("Ticket")] [XmlEnum("27")] Ticket = 27,
+
+        [Description("Outros")][XmlEnum("99")] bcOutros = 99,
     }
 
     /// <summary>
