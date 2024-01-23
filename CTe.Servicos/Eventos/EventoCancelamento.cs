d4fd9995837b61c8c1ff6a37bcd5c1c4788f1379
Flávio Vodzinski <flaviovodzinski@outlook.com>
2024-01-22 21:38:01 -0300

    cte os

--- CTe.Servicos/Eventos/EventoCancelamento.cs
+++ CTe.Servicos/Eventos/EventoCancelamento.cs
@@ -38,22 +38,33 @@ using CTe.Classes.Servicos.Evento.Flags;
 using CTe.Servicos.Factory;
 using CTe.Utils.CTe;
 using CteEletronico = CTe.Classes.CTe;
+using CteEletronicoOS = CTe.CTeOSClasses.CTeOS;
 
 namespace CTe.Servicos.Eventos
 {
     public class EventoCancelamento
     {
         private readonly CteEletronico _cte;
+        private readonly CteEletronicoOS _cteOs;
+
         private readonly int _sequenciaEvento;
         private readonly string _numeroProtocolo;
         private readonly string _justificativa;
-       
+
         public eventoCTe EventoEnviado { get; private set; }
         public retEventoCTe RetornoSefaz { get; private set; }
-        
+
         public EventoCancelamento(CteEletronico cte, int sequenciaEvento, string numeroProtocolo, string justificativa)
         {
             _cte = cte;
+            _sequenciaEvento = sequenciaEvento;
+            _numeroProtocolo = numeroProtocolo;
+            _justificativa = justificativa;
+        }
+
+        public EventoCancelamento(CteEletronicoOS cte, int sequenciaEvento, string numeroProtocolo, string justificativa)
+        {
+            _cteOs = cte;
             _sequenciaEvento = sequenciaEvento;
             _numeroProtocolo = numeroProtocolo;
             _justificativa = justificativa;
@@ -65,6 +76,16 @@ namespace CTe.Servicos.Eventos
 
             EventoEnviado = FactoryEvento.CriaEvento(CTeTipoEvento.Cancelamento, _sequenciaEvento, _cte.Chave(), _cte.infCte.emit.CNPJ, evento, configuracaoServico);
             RetornoSefaz = new ServicoController().Executar(_cte, _sequenciaEvento, evento, CTeTipoEvento.Cancelamento, configuracaoServico);
+
+            return RetornoSefaz;
+        }
+
+        public retEventoCTe CancelarOs(ConfiguracaoServico configuracaoServico = null)
+        {
+            var evento = ClassesFactory.CriaEvCancCTe(_justificativa, _numeroProtocolo);
+
+            EventoEnviado = FactoryEvento.CriaEvento(CTeTipoEvento.Cancelamento, _sequenciaEvento, _cteOs.Chave(), _cteOs.InfCte.emit.CNPJ, evento, configuracaoServico);
+            RetornoSefaz = new ServicoController().Executar(_cteOs, _sequenciaEvento, evento, CTeTipoEvento.Cancelamento, configuracaoServico);
 
             return RetornoSefaz;
         }
---
+++ CTe.Servicos/Recepcao/ServicoCTeOSRecepcao.cs
@@ -0,0 +1,43 @@
+﻿using CTe.Classes;
+using CTe.CTeOSDocumento.CTe.CTeOS.Extensoes;
+using CTe.CTeOSDocumento.CTe.CTeOS.Servicos.Autorizacao;
+using CTe.Servicos.Enderecos.Helpers;
+using CTe.Servicos.Factory;
+using CTe.Utils.CTe;
+using System;
+using System.Text;
+using CTeEletronico = CTe.CTeOSClasses.CTeOS;
+
+namespace CTe.Servicos.Recepcao
+{
+    public class ServicoCTeOSRecepcao
+    {
+        public event EventHandler<AntesEnviarRecepcao> AntesDeEnviar;
+
+        public retCTeOS CTeRecepcaoSincronoV4(CTeEletronico cte, ConfiguracaoServico configuracaoServico = null)
+        {
+            var instanciaConfiguracao = configuracaoServico ?? ConfiguracaoServico.Instancia;
+
+            cte.InfCte.ide.tpEmis = instanciaConfiguracao.TipoEmissao;
+            cte.Assina(instanciaConfiguracao);
+            cte.infCTeSupl = cte.QrCode(instanciaConfiguracao.X509Certificate2, Encoding.UTF8, instanciaConfiguracao.IsAdicionaQrCode, UrlHelper.ObterUrlQrCode(instanciaConfiguracao));
+            cte.SalvarXmlEmDisco(instanciaConfiguracao); //salva em disco antes de validas os schemas, facilitando encontrar possíveis erros
+            cte.ValidaSchema(instanciaConfiguracao);
+            cte.SalvarXmlEmDisco(instanciaConfiguracao);
+
+            var webService = WsdlFactory.CriaWsdlCteRecepcaoSincronoOSV4(configuracaoServico);
+
+            //OnAntesDeEnviar(enviCte);
+
+            var retornoXml = webService.CTeRecepcaoSincV4(cte.CriaRequestWs(configuracaoServico));
+
+            var retorno = retCTeOS.LoadXml(retornoXml.OuterXml, cte);
+
+            retorno.SalvarXmlEmDisco(cte.Chave(), configuracaoServico);
+
+            return retorno;
+        }
+
+
+    }
+}
