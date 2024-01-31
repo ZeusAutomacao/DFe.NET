5df8c434ff7afa893aed7f9b0828554538221d1d
juliocomendes <61715761+juliocomendes@users.noreply.github.com>
2024-01-31 10:51:10 -0300

    Update card.cs
    Adicionado as tags CNPJReceb , idTermPag para atualização fiscal previsto para homologação em 05/02/2024

--- NFe.Classes/Informacoes/Pagamento/card.cs
+++ NFe.Classes/Informacoes/Pagamento/card.cs
@@ -64,5 +64,15 @@ namespace NFe.Classes.Informacoes.Pagamento
         ///     YA07 - Número de autorização da operação cartão de crédito e/ou débito
         /// </summary>
         public string cAut { get; set; }
+
+        /// <summary>
+        ///     YA10 - CNPJ do estabelecimento beneficiário do pagamento
+        /// </summary>
+        public string CNPJReceb { get; set; }
+
+        /// <summary>
+        ///     YA11 - Identificador do terminal em que foi realizado o pagamento
+        /// </summary>
+        public string idTermPag { get; set; }
     }
 }
