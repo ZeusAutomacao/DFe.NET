f5fb41e6646aae9aa9028bb902c1799b7921481b
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2024-04-25 08:41:38 -0300

    fix: impressões que dá para subir no nuget

---
+++ NFe.Danfe.PdfClown/Elementos/LinhaTracejada.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+using NFe.Danfe.PdfClown.Graphics;
+using NFe.Danfe.PdfClown.Tools;
+
+namespace NFe.Danfe.PdfClown.Elementos
+{
+    internal class LinhaTracejada : DrawableBase
+    {
+        public float Margin { get; set; }
+        public double[] DashPattern { get; set; }
+
+        public LinhaTracejada(float margin)
+        {
+            Margin = margin;
+        }
+
+        public override void Draw(Gfx gfx)
+        {
+            base.Draw(gfx);
+
+            gfx.PrimitiveComposer.BeginLocalState();
+            gfx.PrimitiveComposer.SetLineDash(new org.pdfclown.documents.contents.LineDash(new double[] { 3, 2 }));
+            gfx.PrimitiveComposer.DrawLine(new PointF(BoundingBox.Left, Y + Margin).ToPointMeasure(), new PointF(BoundingBox.Right, Y + Margin).ToPointMeasure());
+            gfx.PrimitiveComposer.Stroke();
+            gfx.PrimitiveComposer.End();
+
+        }
+
+        public override float Height { get => 2 * Margin; set => throw new NotSupportedException(); }
+    }   
+}
