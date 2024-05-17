28c1007e19ccd5d3a6123fe270f0b33ccc7fd9ba
André Batista <andrebtsa@gmail.com>
2024-05-17 15:54:39 -0300

    Adicionado tratamento para obtenção do valor do campo dhIniViagem em MDFeIde a depender da versão do leiaute;
    Renomeado infBanc para MDFeInfBanc e adicionado campo PIX;
    
    Adicionado enum MDFeIndAltoDesemp;
    
    Renomeado infPag para MDFeInfPag;
    
    Renomeado infPrazo para MDFeInfPrazo;

---
+++ MDFe.Classes/Flags/MDFeIndAltoDesemp.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Serialization;
+
+namespace MDFe.Classes.Flags
+{
+    public enum MDFeIndAltoDesemp
+    {
+        [XmlEnum("0")]
+        NaoAltoDesempenho = 0,
+        [XmlEnum("1")]
+        AltoDesempenho = 1
+    }
+}
--- MDFe.Servicos/EventosMDFe/EventoPagamentoOperacao.cs
+++ MDFe.Servicos/EventosMDFe/EventoPagamentoOperacao.cs
@@ -10,7 +10,7 @@ namespace MDFe.Servicos.EventosMDFe
     public class EventoPagamentoOperacao
     {
         public MDFeRetEventoMDFe MDFeEventoPagamentoOperacao(Classes.Informacoes.MDFe mdfe, byte sequencia,
-            string protocolo, infViagens infViagens, List<infPag> infPagamentos)
+            string protocolo, infViagens infViagens, List<MDFeInfPag> infPagamentos)
         {
             var eventoPagamento = ClassesFactory.CriaEvPagtoOperMDFe(
                 protocolo
