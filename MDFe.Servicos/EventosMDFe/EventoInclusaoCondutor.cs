d1e4ccec509d6e13a79fa07e36c86496f62a3fe5
André Batista <andrebtsa@gmail.com>
2024-05-21 11:19:41 -0300

    Adicionado parâmetro opcional MDFeConfiguracao em métodos das classes EventoCancelar, EventoEncerramento, EventoInclusaoCondutor, EventoInclusaoDFe, EventoPagamentoOperacao, ServicoMDFeEvento, ServicoMDFeRecepcao, ServicoMDFeRetRecepcao e ServicoMDFeStatusServico.

--- MDFe.Servicos/EventosMDFe/EventoInclusaoCondutor.cs
+++ MDFe.Servicos/EventosMDFe/EventoInclusaoCondutor.cs
@@ -34,17 +34,20 @@
 using MDFe.Classes.Informacoes.Evento.Flags;
 using MDFe.Classes.Retorno.MDFeEvento;
 using MDFe.Servicos.Factory;
+using MDFe.Utils.Configuracoes;
 using MDFeEletronico = MDFe.Classes.Informacoes.MDFe;
 
 namespace MDFe.Servicos.EventosMDFe
 {
     public class EventoInclusaoCondutor
     {
-        public MDFeRetEventoMDFe MDFeEventoIncluirCondutor(MDFeEletronico mdfe, byte sequenciaEvento, string nome, string cpf)
+        public MDFeRetEventoMDFe MDFeEventoIncluirCondutor(MDFeEletronico mdfe, byte sequenciaEvento, string nome, string cpf, MDFeConfiguracao cfgMdfe = null)
         {
+            var config = cfgMdfe ?? MDFeConfiguracao.Instancia;
+
             var incluirCodutor = ClassesFactory.CriaEvIncCondutorMDFe(nome, cpf);
 
-            var retorno = new ServicoController().Executar(mdfe, sequenciaEvento, incluirCodutor, MDFeTipoEvento.InclusaoDeCondutor);
+            var retorno = new ServicoController().Executar(mdfe, sequenciaEvento, incluirCodutor, MDFeTipoEvento.InclusaoDeCondutor, config);
 
             return retorno;
         }
--- MDFe.Servicos/StatusServicoMDFe/ServicoMDFeStatusServico.cs
+++ MDFe.Servicos/StatusServicoMDFe/ServicoMDFeStatusServico.cs
@@ -28,28 +28,31 @@
 /*                                                                              */
 /* Zeusdev Tecnologia LTDA ME - adenilton@zeusautomacao.com.br                  */
 /* http://www.zeusautomacao.com.br/                                             */
-/* Rua Comendador Francisco josé da Cunha, 111 - Itabaiana - SE - 49500-000     */
+/* Rua Comendador Francisco José da Cunha, 111 - Itabaiana - SE - 49500-000     */
 /********************************************************************************/
 
 using MDFe.Classes.Extensoes;
 using MDFe.Classes.Retorno.MDFeStatusServico;
 using MDFe.Servicos.Factory;
+using MDFe.Utils.Configuracoes;
 
 namespace MDFe.Servicos.StatusServicoMDFe
 {
     public class ServicoMDFeStatusServico
     {
-        public MDFeRetConsStatServ MDFeStatusServico()
+        public MDFeRetConsStatServ MDFeStatusServico(MDFeConfiguracao cfgMdfe = null)
         {
-            var consStatServMDFe = ClassesFactory.CriaConsStatServMDFe();
-            consStatServMDFe.ValidarSchema();
-            consStatServMDFe.SalvarXmlEmDisco();
+            var config = cfgMdfe ?? MDFeConfiguracao.Instancia;
 
-            var webService = WsdlFactory.CriaWsdlMDFeStatusServico();
+            var consStatServMDFe = ClassesFactory.CriaConsStatServMDFe(config);
+            consStatServMDFe.ValidarSchema(config);
+            consStatServMDFe.SalvarXmlEmDisco(config);
+
+            var webService = WsdlFactory.CriaWsdlMDFeStatusServico(config);
             var retornoXml = webService.mdfeStatusServicoMDF(consStatServMDFe.CriaRequestWs());
 
             var retorno = MDFeRetConsStatServ.LoadXml(retornoXml.OuterXml, consStatServMDFe);
-            retorno.SalvarXmlEmDisco();
+            retorno.SalvarXmlEmDisco(config);
 
             return retorno;
 
