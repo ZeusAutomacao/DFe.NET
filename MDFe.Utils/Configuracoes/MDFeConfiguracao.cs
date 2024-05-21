17637bd967e6fef31208726bafbf247e4948a06e
André Batista <andrebtsa@gmail.com>
2024-05-21 16:43:41 -0300

    Movida classe MDFeVersaoWebService para seu próprio arquivo

--- MDFe.Utils/Configuracoes/MDFeConfiguracao.cs
+++ MDFe.Utils/Configuracoes/MDFeConfiguracao.cs
@@ -199,12 +199,4 @@ namespace MDFe.Utils.Configuracoes
             }
         }
     }
-
-    public class MDFeVersaoWebService
-    {
-        public int TimeOut { get; set; }
-        public Estado UfEmitente { get; set; }
-        public TipoAmbiente TipoAmbiente { get; set; }
-        public VersaoServico VersaoLayout { get; set; }
-    }
 }
