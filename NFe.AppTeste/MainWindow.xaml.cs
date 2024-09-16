83fac95f17b16cd5dcc893d8c3abe06c22eb9ba6
Danilo Breda <danilo@bredas.com.br>
2024-09-16 20:00:35 -0300

    adicionado suporte ao CRT MEI para DANFE e exemplos appteste

--- NFe.AppTeste/MainWindow.xaml.cs
+++ NFe.AppTeste/MainWindow.xaml.cs
@@ -1388,7 +1388,7 @@ namespace NFe.AppTeste
 
                         //Caso você resolva utilizar método ObterIcmsBasico(), comente esta proxima linha
                         TipoICMS =
-                            crt == CRT.SimplesNacional
+                            crt == CRT.SimplesNacional || crt == CRT.SimplesNacionalMei
                                 ? InformarCSOSN(Csosnicms.Csosn102)
                                 : InformarICMS(Csticms.Cst00, VersaoServico.Versao310)
                     },
