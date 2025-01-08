bdddf3186ba7ac4ffc907a5b5af258b40dd19b86
Agnaldo Silva <agnaldo.santos24@hotmail.com>
2025-01-08 11:54:34 -0300

    Adicionado testes unitários para arredondamento de valores

---
+++ DFe.Testes/Valores/ValorTesteUnitario.cs
@@ -0,0 +1,20 @@
+﻿using DFe.Testes.Valores.DadosDeTeste;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NFe.Classes;
+
+namespace DFe.Testes.Valores
+{
+    [TestClass]
+    public class ValorTesteUnitario
+    {
+        [TestMethod]
+        [DynamicData(nameof(ValorDadosDeTeste.ObterValoresDecimaisParaArredondar), typeof(ValorDadosDeTeste), DynamicDataSourceType.Method)]
+        public void Teste(decimal valor, int casasDecimais)
+        {
+            var valorArredondadoNfe = Classes.Valor.Arredondar(valor, casasDecimais);
+            var valorArredondadoDfe = Valor.Arredondar(valor, casasDecimais);
+
+            Assert.AreEqual(valorArredondadoNfe, valorArredondadoDfe);
+        }
+    }
+}
