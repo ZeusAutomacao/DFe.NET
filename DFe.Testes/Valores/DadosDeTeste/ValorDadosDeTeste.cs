ab2ce8bf9fec4a1799f4c95b2ce19503b77e52eb
Agnaldo Silva <agnaldo.santos24@hotmail.com>
2025-01-08 11:54:34 -0300

    Adicionado testes unitários para arredondamento de valores

---
+++ DFe.Testes/Valores/DadosDeTeste/ValorDadosDeTeste.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace DFe.Testes.Valores.DadosDeTeste
+{
+    public class ValorDadosDeTeste
+    {
+        public static IEnumerable<object[]> ObterValoresDecimaisParaArredondar()
+        {
+            return new List<object[]>
+            {
+                new object[] { 20.35m * 15.90m, 2 },
+                new object[] { 0.35m * 15.90m, 2 },
+                new object[] { 3.665m, 2 },
+                new object[] { 4.775m, 2 },
+
+                new object[] { 20.35m * 15.90m, 3 },
+                new object[] { 0.35m * 15.90m, 3 },
+                new object[] { 3.665m, 3 },
+                new object[] { 4.7757m, 3 },
+                new object[] { 5.44555m, 3 },
+            };
+        }
+    }
+}
