257bfd4d629ac7398ebd5dd7ee51a1cf15cec3f9
mgpx-work <mgpx1993@gmail.com>
2025-02-28 17:30:57 -0300

    Implementado o CTE simplificado

--- CTe.Classes/Informacoes/Tipos/tpCTe.cs
+++ CTe.Classes/Informacoes/Tipos/tpCTe.cs
@@ -3,7 +3,7 @@ using System.Xml.Serialization;
 namespace CTe.Classes.Informacoes.Tipos
 {
     /// <summary>
-    ///     Tipo do Documento Fiscal (0 - CT-e Normal; 1 - CT-e de Complemento de Valores; 2 - CT-e de Anulação; 3 - CT-e Substituto)
+    ///     Tipo do Documento Fiscal (0 - CT-e Normal; 1 - CT-e de Complemento de Valores; 2 - CT-e de Anulação; 3 - CT-e Substituto; 5 - Simplificado; 6 - Substitudo Simplificado)
     /// </summary>
     public enum tpCTe
     {
@@ -14,6 +14,10 @@ namespace CTe.Classes.Informacoes.Tipos
         [XmlEnum("2")]
         Anulacao,
         [XmlEnum("3")]
-        Substituto
+        Substituto,
+        [XmlEnum("5")]
+        Simplificado,
+        [XmlEnum("6")]
+        SubstitutoSimplificado
     }
 }
---
+++ CTe.Classes/Simplificado/Entrega/detEntrega.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTe.Classes.Simplificado.Entrega
+{
+    internal class detEntrega
+    {
+    }
+}
