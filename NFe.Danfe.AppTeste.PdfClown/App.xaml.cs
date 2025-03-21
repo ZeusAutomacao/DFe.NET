2e08e7d8374086e379dae77a1a1e07b564a7029c
Maurício Bernardo <mauriciobernardodesouza2@hotmail.com>
2025-03-21 14:47:10 -0300

    Adicionado aplicação de teste PdfClown

---
+++ NFe.Danfe.AppTeste.PdfClown/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace NFe.Danfe.AppTeste.PdfClown;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
---
+++ NFe.Danfe.AppTeste.PdfClown/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Win32;
+using NFe.Danfe.PdfClown;
+using NFe.Danfe.PdfClown.Modelo;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+
+namespace NFe.Danfe.AppTeste.PdfClown
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_GerarDanfe_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Arquivos XML (*.xml)|*.xml",
+                Title = "Selecione o XML da NF-e"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                var caminhoXml = openFileDialog.FileName;
+
+                try
+                {
+                    string xml = File.ReadAllText(caminhoXml);
+
+                    byte[]? logo = null;
+
+                    byte[] pdfBytes = GerarDanfeZion(xml, logo);
+
+                    string tempFile = Path.Combine(Path.GetTempPath(), "danfe-nfe.pdf");
+                    File.WriteAllBytes(tempFile, pdfBytes);
+                    Process.Start(new ProcessStartInfo(tempFile) { UseShellExecute = true });
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Erro ao gerar DANFE: {ex.Message}", "Erro", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private static byte[] GerarDanfeZion(string xmlNfeProc, byte[]? logoMarca)
+        {
+            xmlNfeProc = xmlNfeProc.Replace("\u00a0", " ");
+            var model = DanfeViewModelCreator.CriarDeStringXml(xmlNfeProc);
+
+            using var pdfStream = new MemoryStream();
+            using var danfe = new DanfeDoc(model);
+            if (logoMarca != null)
+            {
+                using var logo = new MemoryStream(logoMarca);
+                {
+                    danfe.AdicionarLogoImagem(logo);
+                }
+            }
+
+            danfe.Gerar();
+            return danfe.ObterPdfBytes(pdfStream);
+        }
+    }
+}
