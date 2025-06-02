88751f522e7b2ea993b23a7ee859db7e30da6dad
Agnaldo Silva <agnaldo.santos24@hotmail.com>
2025-06-02 11:31:35 -0300

    test: criado testes unitários para o método ObterHashSha1Bytes na classe AssinaturaDigital e para o método ObterHexSha1DeString da classe ConversaoTesteUnitario

---
+++ DFe.Testes/Utils/Assinatura/AssinaturaDigitalTesteUnitario.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using DadosDeTestes.AssinaturaDigital;
+using DFe.Utils.Assinatura;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DFe.Testes.Utils.Assinatura;
+
+[TestClass]
+public class AssinaturaDigitalTesteUnitario
+{
+    [TestMethod(displayName: "Dado dados para geração do hash sha1 bytes, quando obter hash sha1 bytes, então deve obter hash sha1 em bytes igual do SHA1CryptoServiceProvider.")]
+    [DynamicData(nameof(AssinaturaDigitalTesteDados.ObterDadosParaGeracaoDoHashSha1Bytes), typeof(AssinaturaDigitalTesteDados), DynamicDataSourceType.Method)]
+    public void DadoDadosParaGeracaoDoHashSha1BytesQuandoObterHashSha1BytesEntaoDeveObterHashSha1EmBytesIgualDoSha1CryptoServiceProvider(string dadosEmString)
+    {
+        // Arrange
+        var dadosEmBytes = Encoding.UTF8.GetBytes(dadosEmString);
+        var bytesEsperados = ObterHashSha1StringUsandoSha1CryptoServiceProvider(dadosEmBytes);
+
+        // Act
+        var bytesRetornado = AssinaturaDigital.ObterHashSha1Bytes(dadosEmBytes);
+
+        // Assert
+        CollectionAssert.AreEqual(bytesEsperados, bytesRetornado);
+    }
+
+    private byte[] ObterHashSha1StringUsandoSha1CryptoServiceProvider(byte[] dados)
+    {
+        var sha1 = new SHA1CryptoServiceProvider();
+        var hashSha1Bytes = sha1.ComputeHash(dados);
+
+        return hashSha1Bytes;
+    }
+}
---
+++ DadosDeTestes/AssinaturaDigital/AssinaturaDigitalDadosDeTeste.cs
@@ -0,0 +1,15 @@
+﻿namespace DadosDeTestes.AssinaturaDigital;
+
+public class AssinaturaDigitalTesteDados
+{
+    public static IEnumerable<object[]> ObterDadosParaGeracaoDoHashSha1Bytes()
+    {
+        return new List<object[]>
+        {
+            new object[] { "28250632876302000114650010000122331289270262|2|2|1020228753214A38CE050010AB5C066A9" },
+            new object[] { "28250332876302000114550010000010471000000758" },
+            new object[] { "28250332876302000114550010000010201000000070" },
+            new object[] { "28250332876302000114550010000010871000081770" }
+        };
+    }
+}
