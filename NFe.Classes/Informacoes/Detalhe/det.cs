8c08262300daa0df24a07188415b550319d9e8ef
Vinicius Cunha <vinicius.git36@gmail.com>
2025-07-15 10:36:50 -0300

    feat: Adicionado grupo VB: Total do item da NF-e

--- NFe.Classes/Informacoes/Detalhe/det.cs
+++ NFe.Classes/Informacoes/Detalhe/det.cs
@@ -38,6 +38,8 @@ namespace NFe.Classes.Informacoes.Detalhe
 {
     public class det
     {
+        private decimal _vItem;
+        
         /// <summary>
         ///     H02 - Número do item do NF
         /// </summary>
@@ -69,5 +71,14 @@ namespace NFe.Classes.Informacoes.Detalhe
         /// </summary>
         [XmlElement(nameof(obsItem))]
         public obsItem obsItem { get; set; }
+        
+        /// <summary>
+        ///  VB01 - Valor Total do Item da NF-e
+        /// </summary>
+        public decimal vItem
+        {
+            get => _vItem; 
+            set => _vItem = value.Arredondar(2); 
+        }
     }
 }
