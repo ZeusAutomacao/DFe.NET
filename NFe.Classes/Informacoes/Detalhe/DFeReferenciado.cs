9fc1992ff0ad3eee4c31521012222c6eb550b4b8
Vinicius Cunha <vinicius.git36@gmail.com>
2025-07-15 11:32:16 -0300

    feat: Adicionado grupo VC: Referenciamento de item de outro Documento Fiscal Eletrônico - DF-e

---
+++ NFe.Classes/Informacoes/Detalhe/DFeReferenciado.cs
@@ -0,0 +1,16 @@
+namespace NFe.Classes.Informacoes.Detalhe
+{
+    public class DFeReferenciado
+    {
+        
+        /// <summary>
+        ///  VC02 - Chave de acesso do DF-e referenciado
+        /// </summary>
+        public string chaveAcesso { get; set; }
+
+        /// <summary>
+        ///  VC03 - Número do item do documento referenciado.
+        /// </summary>
+        public int? nItem { get; set; }
+    }
+}
