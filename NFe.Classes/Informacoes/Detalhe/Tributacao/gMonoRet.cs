f721318ef0d298e22ac19d6698e85b2e2fae91c4
Roberto Alves <robertoalves@multimarcaseditoriais.com.br>
2025-08-11 12:20:41 -0300

    feat: layout reforma tributária

---
+++ NFe.Classes/Informacoes/Detalhe/Tributacao/gMonoRet.cs
@@ -0,0 +1,46 @@
+﻿namespace NFe.Classes.Informacoes.Detalhe.Tributacao
+{
+    public class gMonoRet
+    {
+        private decimal _qBcMonoRet;
+        private decimal _adRemIbsRet;
+        private decimal _vIbsMonoRet;
+        private decimal _adRemCbsRet;
+        private decimal _vCbsMonoRet;
+
+        // UB95
+        public decimal qBCMonoRet
+        {
+            get => _qBcMonoRet.Arredondar(4);
+            set => _qBcMonoRet = value.Arredondar(4);
+        }
+
+        // UB96
+        public decimal adRemIBSRet
+        {
+            get => _adRemIbsRet.Arredondar(4);
+            set => _adRemIbsRet = value.Arredondar(4);
+        }
+
+        // UB97
+        public decimal vIBSMonoRet
+        {
+            get => _vIbsMonoRet.Arredondar(2);
+            set => _vIbsMonoRet = value.Arredondar(2);
+        }
+
+        // UB98
+        public decimal adRemCBSRet
+        {
+            get => _adRemCbsRet.Arredondar(4);
+            set => _adRemCbsRet = value.Arredondar(4);
+        }
+
+        // UB98a
+        public decimal vCBSMonoRet
+        {
+            get => _vCbsMonoRet.Arredondar(2);
+            set => _vCbsMonoRet = value.Arredondar(2);
+        }
+    }
+}
---
+++ NFe.Classes/Informacoes/Total/IBSCBSTot.cs
@@ -0,0 +1,23 @@
+﻿namespace NFe.Classes.Informacoes.Total
+{
+    public class IBSCBSTot
+    {
+        private decimal _vBCIBSCBS;
+
+        // W35
+        public decimal vBCIBSCBS
+        {
+            get => _vBCIBSCBS;
+            set => _vBCIBSCBS = value.Arredondar(2);
+        }
+
+        // W36
+        public gIBS gIBS { get; set; }
+
+        // W50
+        public gCBSTotal gCBS { get; set; }
+
+        // W57
+        public gMono gMono { get; set; }
+    }
+}
