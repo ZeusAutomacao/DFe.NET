c49d220b3a523d65d25b9bf207c9a72c9f18efce
Walisson Pires R. Miranda <walissonpiresgv@hotmail.com>
2025-08-10 23:09:21 -0300

    NT2024.003v1.03: Grupo defensivos em agropecuario passou permitir multiplas ocorrências (#90)

--- NFe.Classes/Informacoes/Agropecuario/agropecuario.cs
+++ NFe.Classes/Informacoes/Agropecuario/agropecuario.cs
@@ -1,36 +1,39 @@
-﻿namespace NFe.Classes.Informacoes.Agropecuario
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace NFe.Classes.Informacoes.Agropecuario
 {
     public class agropecuario
     {
 #if NET5_0_OR_GREATER//o uso de tipos de referência anuláveis não é permitido até o C# 8.0.
 
         /// <summary>
-        /// ZF02 - serieGuia
+        /// ZF02 - Defensivos Agrícolas
         /// </summary>
-        public defensivo? defensivo { get; set; }
+        [XmlElement("defensivo")]
+        public List<defensivo>? defensivo { get; set; }
 
         /// <summary>
         /// ZF04 - Guia de Trânsito
         /// </summary>
+        [XmlElement("guiaTransito")]
         public guiaTransito? guiaTransito { get; set; }
 
-        public bool ShouldSerializedefensivo()
-        {
-            return defensivo != null;
-        }
         public bool ShouldSerializeguiaTransito()
         {
             return guiaTransito != null;
         }
 #else
         /// <summary>
-        /// ZF02 - serieGuia
+        /// ZF02 - Defensivos Agrícolas
         /// </summary>
-        public defensivo defensivo { get; set; }
+        [XmlElement("defensivo")]
+        public List<defensivo> defensivo { get; set; }
 
         /// <summary>
         /// ZF04 - Guia de Trânsito
         /// </summary>
+        [XmlElement("guiaTransito")]
         public guiaTransito guiaTransito { get; set; }
 #endif
     }
